1d14fcfa001068fef05c53d2bd5dad8296bc8f5e
Victor Estrade <victor.estrade@lri.fr>
2019-03-07 17:31:45 +0100

    Run script and main

---
+++ main.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import argparse
+import inspect
+import logging
+import iminuit
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# FIXME : change a module name is evil
+import higgs_geant as problem
+
+from sklearn.model_selection import ShuffleSplit
+from higgs_geant import normalize_weight
+from higgs_geant import split_train_test
+from higgs_geant import split_data_label_weights
+from higgs_4v_pandas import tau_energy_scale
+from nll import HiggsModel
+from models import MODELS
+ARG_MODELS = MODELS.keys()
+
+def parse_args():
+    # TODO : more descriptive msg.
+    parser = argparse.ArgumentParser(description="Training launcher")
+
+    parser.add_argument("--verbosity", "-v", type=int, choices=[0, 1, 2],
+                        default=0, help="increase output verbosity")
+
+    # DATASET CHOICE
+    # parser.add_argument('--data', help='chosen dataset',
+                        # type=str, choices=PROBLEMS.keys(), default='mnist' )
+
+    # MODEL CHOICE
+    parser.add_argument('model', help='model to train',
+                        type=str, choices=ARG_MODELS )
+
+    # MODEL HYPER PARAMETERS
+    parser.add_argument('--n-estimators', help='number of estimators',
+                        default=1000, type=int)
+
+    parser.add_argument('--max-depth', help='maximum depth of trees',
+                        default=3, type=int)
+
+    parser.add_argument('--learning-rate', '--lr', help='learning rate',
+                        default=1e-3, type=float)
+
+    parser.add_argument('--trade-off', help='trade-off for multi-objective models',
+                        default=1.0, type=float)
+
+    parser.add_argument('-w', '--width', help='width for the data augmentation sampling',
+                        default=5, type=float)
+
+    parser.add_argument('--batch-size', help='mini-batch size',
+                        default=128, type=int)
+
+    parser.add_argument('--n-steps', help='number of update steps',
+                        default=10000, type=int)
+
+    parser.add_argument('--n-augment', help='number of times the dataset is augmented',
+                        default=2, type=int)
+
+    parser.add_argument('--n-adv-pre-training-steps',
+                        help='number of update steps for the pre-training',
+                        default=3000, type=int)
+
+    parser.add_argument('--n-clf-pre-training-steps',
+                        help='number of update steps for the pre-training',
+                        default=3000, type=int)
+
+    parser.add_argument('--n-recovery-steps',
+                        help='number of update steps for the catch training of auxiliary models',
+                        default=5, type=int)
+
+    parser.add_argument('--fraction-signal-to-keep',
+                        help='fraction of signal to keep in Filters',
+                        default=0.95, type=float)
+
+    # OTHER
+    parser.add_argument('--no-cuda', '--no-gpu', help='flag to use or not the gpu',
+                        action='store_false', dest='cuda')
+
+    args = parser.parse_args()
+    return args
+
+
+def get_model_class(data_name, model_name):
+    model_class = None
+    if data_name in MODELS:
+        model_class = MODELS[data_name](model_name)
+    else:
+        raise ValueError('Unrecognized dataset name : {}'
+                         'Expected one from {}'. format(data_name, MODELS.keys()))
+    return model_class
+
+
+def extract_model_args(args, get_model):
+    sig = inspect.signature(get_model)
+    args_dict = vars(args)
+    model_args = { k: args_dict[k] for k in sig.parameters.keys() if k in args_dict.keys() }
+    return model_args
+
+
+
+def get_cv_iter(X, y):
+    cv = ShuffleSplit(n_splits=12, test_size=0.2, random_state=config.RANDOM_STATE)
+    cv_iter = list(cv.split(X, y))
+    return cv_iter
+
+# =====================================================================
+# MAIN
+# =====================================================================
+def main():
+    # GET LOGGER
+    #-----------
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+    logger.addHandler(stream_handler)
+    logger.info('Hello')
+
+    args = parse_args()
+    logger.info(args)
+
+    # GET CHOSEN MODEL
+    #-----------------
+    logger.info('Building model ...')
+    logger.info( 'Model :', args.model)
+    model_class = get_model_class(args.data, args.model)
+    # args.skewing_function = problem.skew
+    # args.tangent = problem.tangent
+    model_args = extract_model_args(args, model_class)
+    logger.info( 'model_args :', model_args )
+    model = model_class(**model_args)
+
+    logger.info('Loading data ...')
+    data = problem.load_data()
+
+    # SPLIT DATA
+    #-----------
+    cv = ShuffleSplit(n_splits=1, test_size=0.2, random_state=config.RANDOM_STATE)
+    idx_sim, idx_xp = next(cv.split(data, data['Label']))
+    data_sim, data_xp = split_train_test(data, idx_sim, idx_xp)
+
+    cv = ShuffleSplit(n_splits=1, test_size=0.2, random_state=config.RANDOM_STATE)
+    idx_train, idx_test = next(cv.split(data_sim, data_sim['Label']))
+    data_train, data_test = split_train_test(data_sim, idx_train, idx_test)
+
+    data_train = data_train.copy()
+    data_test = data_test.copy()
+    data_xp = data_xp.copy()
+    data_train['Weight'] = normalize_weight(data_train['Weight'], data_train['Label'])
+    data_test['Weight'] = normalize_weight(data_test['Weight'], data_test['Label'])
+    data_xp['Weight'] = normalize_weight(data_xp['Weight'], data_xp['Label'])
+
+    X_train, y_train, W_train = split_data_label_weights(data_train)
+    X_test, y_test, W_test = split_data_label_weights(data_test)
+    X_xp, y_xp, W_xp = split_data_label_weights(data_test)
+
+    # TRAIN MODEL
+    #------------
+    logger.info('Start training submission : {}'.format(model.get_name()))
+    model.fit(X_train, y_train, sample_weight=W_train)
+    logger.info('End of training {}'.format(model.get_name()))
+
+    # CHECK TRAINING RESULT
+    #----------------------
+    proba = model.predict_proba(X_test)
+    sns.distplot(proba[y_test==0, 1], label='b')
+    sns.distplot(proba[y_test==1, 1], label='s')
+    plt.legend()
+    # FIXME : name depend on model name and cv_iter
+    plt.savefig('test_distrib.png')
+
+
+    X_infer = X_xp.copy()
+    W_infer = W_xp.copy()
+    TRUE_TAU_ES = 1.03
+    tau_energy_scale(X_infer, scale=TRUE_TAU_ES)
+    N_BIN = 20
+    negative_log_likelihood = HiggsModel(model, X_test, y_test, W_test, X_infer, W_infer, N_BIN=N_BIN)
+    minimizer = iminuit.Minuit(negative_log_likelihood,
+                    errordef=ERRORDEF_NLL,
+                    )
+
+    with np.warnings.catch_warnings():
+        np.warnings.filterwarnings('ignore', message='.*arcsinh')
+        fmin, param = minimizer.migrad()
+
+    # What if mingrad failed ?
+    valid = minimizer.migrad_ok()
+    logger.info("Minigrad OK ? {}".format(valid) )
+
+    # Compute hessian error
+    with np.warnings.catch_warnings():
+        np.warnings.filterwarnings('ignore', message='.*arcsinh')
+        param = minimizer.hesse()
+    logger.info("fitarg = {} ".format(param) )
+
+    # Stuff to save
+    fitarg = minimizer.fitarg
+    logger.info("fitarg = {} ".format(fitarg) )
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ run_array.py
@@ -0,0 +1,190 @@
+import os
+import sys
+import argparse
+import datetime
+import itertools
+
+import pandas as pd
+
+from subprocess import call
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Job launcher")
+
+    parser.add_argument('--logdir', help='path to the log directory',
+                        default='logs')
+    parser.add_argument('--xp-name', help='name of the experiment',
+                        default='Trololo')
+    parser.add_argument('--docker-image', help='the docker image',
+                        default='estradevictorantoine/systml:1.0', type=str)
+
+    # max runtime days-hours:min:sec
+    ressources = parser.add_argument_group('ressources', 'ressources taken by every job')
+    ressources.add_argument('--max-time', help='maximum job time',
+                        default='2-00:00:00')
+    ressources.add_argument('--cpu', help='number of allocated CPUs',
+                        default=6, type=int)
+    ressources.add_argument('--mem', help='allocated RAM',
+                        default='64g')
+    ressources.add_argument('--partition', help='chosen partition on the cluster',
+                        default='titanic')
+    ressources.add_argument('--gpu', help='number of allocated GPUs',
+                        default=0, type=int)
+
+    # main arguments
+    main_args = parser.add_argument_group('main_args', 'arguments passed to the subjobs for grid search')
+    main_args.add_argument('--n-estimators',
+                        nargs='+',
+                        help='number of estimators',
+                        default=1000, type=int)
+
+    main_args.add_argument('--max-depth',
+                        nargs='+',
+                        help='maximum depth of trees',
+                        default=3, type=int)
+
+    main_args.add_argument('--learning-rate', '--lr',
+                        nargs='+',
+                        help='learning rate',
+                        default=1e-3, type=float)
+
+    main_args.add_argument('--trade-off',
+                        nargs='+',
+                        help='trade-off for multi-objective models',
+                        default=1.0, type=float)
+
+    main_args.add_argument('-w', '--width',
+                        nargs='+',
+                        help='width for the data augmentation sampling',
+                        default=5, type=float)
+
+    main_args.add_argument('--batch-size',
+                        nargs='+',
+                        help='mini-batch size',
+                        default=128, type=int)
+
+    main_args.add_argument('--n-steps',
+                        nargs='+',
+                        help='number of update steps',
+                        default=10000, type=int)
+
+    main_args.add_argument('--n-augment',
+                        nargs='+',
+                        help='number of times the dataset is augmented',
+                        default=2, type=int)
+
+    main_args.add_argument('--n-adv-pre-training-steps',
+                        nargs='+',
+                        help='number of update steps for the pre-training',
+                        default=3000, type=int)
+
+    main_args.add_argument('--n-clf-pre-training-steps',
+                        nargs='+',
+                        help='number of update steps for the pre-training',
+                        default=3000, type=int)
+
+    main_args.add_argument('--n-recovery-steps',
+                        nargs='+',
+                        help='number of update steps for the catch training of auxiliary models',
+                        default=5, type=int)
+
+    args = parser.parse_args()
+    main_args_dict = extract_group_args(parser, args, 'main_args')
+
+    return args, main_args_dict
+
+
+def extract_group_args(parser, args, group_title):
+    parser_groups = {group.title:{action.option_strings[0]:getattr(args,action.dest,None) 
+                        for action in group._group_actions} 
+                    for group in parser._action_groups}
+    group_args = parser_groups[group_title]
+    return group_args
+
+
+SBATCH_TEMPLATE = \
+"""#!/bin/sh
+#SBATCH --account=tau
+#SBATCH --array={array}%10
+#SBATCH --job-name={xp_name}
+#SBATCH --output={log_stdout}
+#SBATCH --error={log_stderr}
+#SBATCH -t {max_time}             # max runtime days-hours:min:sec
+#SBATCH --cpus-per-task={cpu}
+#SBATCH --mem={memory}
+#SBATCH --partition={partition}
+#SBATCH --gres=gpu:{gpu}
+
+GRID_PARAMS=$(cat {parameters_file} | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
+WORKDIR="/home/tao/vestrade/workspace/SystML/"
+
+sdocker -i  -v /data/titanic_3/users/vestrade/datawarehouse:/datawarehouse \
+            -v /data/titanic_3/users/vestrade/savings:/data/titanic_3/users/vestrade/savings \
+            -v $WORKDIR:$WORKDIR \
+            {docker_image} \
+            /bin/sh -c "cd ${{WORKDIR}}; python main.py ${{GRID_PARAMS}}" 
+"""
+
+
+def param_to_grid(parameter_dict):
+    params = itertools.product(*parameter_dict.values())
+    param_names = parameter_dict.keys()
+    grid = [{k:v for k, v in zip(param_names, v_list)} for v_list in params]
+    return grid
+
+
+def grid_to_str(grid):
+    return "\n".join([' '.join(['{} {}'.format(k, v) for k, v in d.items()]) for d in grid])
+
+
+def to_list(l):
+    try:
+        iter(l)
+        return l
+    except TypeError:
+        return [l]
+
+
+def main():
+    # Extract arguments :
+    args, main_args = parse_args()
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")    
+    xp_name = args.xp_name
+    logdir = os.path.join(args.logdir, xp_name, now)
+    max_time = args.max_time
+    cpu = args.cpu
+    memory = args.mem
+    partition = args.partition
+    gpu = args.gpu
+    docker_image = args.docker_image
+
+    # Main parameters for grid search
+    parameter_dict = {k:to_list(v) for k, v in main_args.items()}
+    grid = param_to_grid(parameter_dict)
+    array = "1-{}".format(len(grid))
+
+    # Extra arguments
+    log_stdout = os.path.join(logdir, '%A_%a.stdout')
+    log_stderr = os.path.join(logdir, '%A_%a.stderr')
+    script_slurm = os.path.join(logdir, 'script.slurm')
+    parameters_file = os.path.join(logdir, 'parameters.txt')
+    parameters_file_csv = os.path.join(logdir, 'parameters.csv')
+
+    # Final formating
+    script = SBATCH_TEMPLATE.format(**locals())
+
+    # Prepare logs files, slurm script, parameters, etc
+    os.makedirs(logdir, exist_ok=True)
+    with open(script_slurm, "w") as file:
+        print(script, file=file)
+    # Prepare parameter grid file
+    with open(parameters_file, "w") as file:
+        print(grid_to_str(grid), file=file)
+    pd.DataFrame(grid).to_csv(parameters_file_csv, index=False)
+
+    # Start job
+    call(['sbatch', script_slurm])
+
+if __name__ == '__main__':
+    main()
