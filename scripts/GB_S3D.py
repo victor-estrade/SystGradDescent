6220f821b0ce43b3f00abdd13f23dba9949d2749
Victor Estrade <victor.estrade@lri.fr>
2019-09-17 14:12:09 +0200

    Add command line reminder

--- scripts/GB_S3D.py
+++ scripts/GB_S3D.py
@@ -5,6 +5,8 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+# Command line : 
+# python -m scripts.GB_S3D 
 
 import os
 import argparse
