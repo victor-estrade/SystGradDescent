29631d2b40be59ba6adab0de1f723c9b55b72d43
Victor Estrade <victor.estrade@lri.fr>
2019-09-24 14:44:15 +0200

    done refactoring

--- benchmark/S3D2_GB.py
+++ benchmark/S3D2_GB.py
@@ -9,13 +9,10 @@ from __future__ import unicode_literals
 # python -m benchmark.GB_S3D 
 
 import os
-import argparse
 import logging
 import config
 import iminuit
 ERRORDEF_NLL = 0.5
-
-import numpy as np
 
 
 from utils import set_plot_config
@@ -26,13 +23,13 @@ from utils import print_line
 from utils import get_model
 from utils import print_params
 
-from myplot import plot_test_distrib
+from myplot import plot_valid_distrib
+from myplot import plot_summaries
 from myplot import plot_params
 
-from problem.synthetic3D import Synthetic3D
-from problem.synthetic3D import Config
-from problem.synthetic3D import split_data_label_weights
-from problem.synthetic3D import Synthetic3DNLL
+from problem.synthetic3D import S3D2Config
+from problem.synthetic3D import S3D2
+from problem.synthetic3D import S3D2NLL
 
 from .S3D2_utils import plot_R_around_min
 from .S3D2_utils import plot_LAMBDA_around_min
@@ -67,12 +64,12 @@ def run(args, i_cv):
     print_line()
     # LOAD/GENERATE DATA
     logger.info('Set up data generator')
-    pb_config = Config()
+    pb_config = S3D2Config()
     seed = config.SEED + i_cv * 5
-    # TODO
-    # generator = Synthetic3D( seed=seed,  n_expected_events=1050 )
-    # generator.N_SIG = pb_config.N_SIG
-    # generator.N_BKG = pb_config.N_BKG
+    train_generator = S3D2(seed)
+    valid_generator = S3D2(seed+1)
+    test_generator  = S3D2(seed+2)
+
 
     # SET MODEL
     logger.info('Set up classifier')
@@ -80,12 +77,11 @@ def run(args, i_cv):
     
     # TRAINING
     logger.info('Generate training data')
-    # TODO
-    # D_train = generator.train_sample(pb_config.CALIBRATED_R,
-    #                                  pb_config.CALIBRATED_LAMBDA,
-    #                                  pb_config.CALIBRATED_MU,
-    #                                  n_samples=pb_config.N_TRAINING_SAMPLES)
-    # X_train, y_train, w_train = split_data_label_weights(D_train)
+    X_train, y_train, w_train = train_generator.generate(
+                                     pb_config.CALIBRATED_R,
+                                     pb_config.CALIBRATED_LAMBDA,
+                                     pb_config.CALIBRATED_MU,
+                                     n_samples=pb_config.N_TRAINING_SAMPLES)
     logger.info('Training {}'.format(model.get_name()))
     model.fit(X_train, y_train, w_train)
     logger.info('Training DONE')
@@ -97,17 +93,14 @@ def run(args, i_cv):
     os.makedirs(model_path, exist_ok=True)
     model.save(model_path)
 
+
     # CHECK TRAINING
     logger.info('Generate validation data')
-    # TODO
-    # D_test = generator.test_sample(pb_config.CALIBRATED_R,
-    #                                pb_config.CALIBRATED_LAMBDA,
-    #                                pb_config.CALIBRATED_MU)
-    # D_final = generator.final_sample(pb_config.TRUE_R,
-    #                                pb_config.TRUE_LAMBDA,
-    #                                pb_config.TRUE_MU)
-    # X_test, y_test, w_test = split_data_label_weights(D_test)
-    # X_final, y_final, w_final = split_data_label_weights(D_final) 
+    X_valid, y_valid, w_valid = valid_generator.generate(
+                                     pb_config.CALIBRATED_R,
+                                     pb_config.CALIBRATED_LAMBDA,
+                                     pb_config.CALIBRATED_MU,
+                                     n_samples=pb_config.N_VALIDATION_SAMPLES)
     
     logger.info('Plot distribution of the score')
     plot_valid_distrib(model, model_name, model_path, X_valid, y_valid, classes=("b", "s"))
@@ -115,14 +108,15 @@ def run(args, i_cv):
 
     # MEASUREMENT
     logger.info('Generate testing data')
-    # TODO
-    # true_apple_ratio = 0.8
-    # param_truth = [true_apple_ratio]
-    X_test, y_test, w_test = test_generator.generate(apple_ratio=true_apple_ratio, n_samples=2_000)
+    X_test, y_test, w_test = test_generator.generate(
+                                     pb_config.TRUE_R,
+                                     pb_config.TRUE_LAMBDA,
+                                     pb_config.TRUE_MU,
+                                     n_samples=pb_config.N_TESTING_SAMPLES)
 
     logger.info('Set up NLL computer')
-    compute_summaries = ClassifierSummaryComputer(model)
-    compute_nll = Synthetic3DNLL(summary_computer, generator, X_final, w_final)
+    compute_summaries = ClassifierSummaryComputer(model, n_bins=10)
+    compute_nll = S3D2NLL(compute_summaries, valid_generator, X_test, w_test)
 
     logger.info('Plot summaries')
     plot_summaries(compute_summaries, model_name, model_path, 
@@ -132,47 +126,42 @@ def run(args, i_cv):
 
     # NLL PLOTS
     logger.info('Plot NLL around minimum')
-    # TODO ???
-    # plot_R_around_min(compute_nll, pb_config, model_path)
-    # plot_LAMBDA_around_min(compute_nll, pb_config, model_path)
-    # plot_MU_around_min(compute_nll, pb_config, model_path)
+    plot_R_around_min(compute_nll, pb_config, model_path)
+    plot_LAMBDA_around_min(compute_nll, pb_config, model_path)
+    plot_MU_around_min(compute_nll, pb_config, model_path)
 
     # MINIMIZE NLL
     logger.info('Prepare minuit minimizer')
-    # TODO :
-    # start_params
-    # error_params
-    # minimizer = iminuit.Minuit(compute_nll,
-    #                            errordef=ERRORDEF_NLL,
-    #                            r=pb_config.CALIBRATED_R,
-    #                            error_r=pb_config.CALIBRATED_R_ERROR,
-    #                            #limit_r=(0, None),
-    #                            lam=pb_config.CALIBRATED_LAMBDA,
-    #                            error_lam=pb_config.CALIBRATED_LAMBDA_ERROR,
-    #                            limit_lam=(0, None),
-    #                            mu=pb_config.CALIBRATED_MU,
-    #                            error_mu=pb_config.CALIBRATED_MU_ERROR,
-    #                            limit_mu=(0, 1),
-    #                           )
+    minimizer = iminuit.Minuit(compute_nll,
+                               errordef=ERRORDEF_NLL,
+                               r=pb_config.CALIBRATED_R,
+                               error_r=pb_config.CALIBRATED_R_ERROR,
+                               #limit_r=(0, None),
+                               lam=pb_config.CALIBRATED_LAMBDA,
+                               error_lam=pb_config.CALIBRATED_LAMBDA_ERROR,
+                               limit_lam=(0, None),
+                               mu=pb_config.CALIBRATED_MU,
+                               error_mu=pb_config.CALIBRATED_MU_ERROR,
+                               limit_mu=(0, 1),
+                              )
     minimizer.print_param()
     logger.info('Mingrad()')
-    fmin, param = minimizer.migrad()
+    fmin, params = minimizer.migrad()
     logger.info('Mingrad DONE')
+    params_truth = [pb_config.TRUE_R, pb_config.TRUE_LAMBDA, pb_config.TRUE_MU]
 
     if minimizer.migrad_ok():
         logger.info('Mingrad is VALID !')
-        print_params(param, param_truth)
+        print_params(params, params_truth)
         logger.info('Hesse()')
-        param = minimizer.hesse()
+        params = minimizer.hesse()
         logger.info('Hesse DONE')
 
     logger.info('Plot params')
-    # TODO
-    # params_truth = [pb_config.TRUE_R, pb_config.TRUE_LAMBDA, pb_config.TRUE_MU]
-    plot_params(param, params_truth, model_name, model_path)
+    plot_params(params, params_truth, model_name, model_path)
 
-    print(param[2]['value'] * 1050, 'signal events estimated')
-    print(param[2]['error'] * 1050, 'error on # estimated sig event')
+    print(params[2]['value'] * 1050, 'signal events estimated')
+    print(params[2]['error'] * 1050, 'error on # estimated sig event')
     print('Done.')
 
 
