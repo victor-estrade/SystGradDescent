50151fa6efe01c4846d7d6ee728fea653342b083
Victor Estrade <victor.estrade@lri.fr>
2019-09-24 12:07:48 +0200

    Add plot summaries and plot valid distrib

--- myplot.py
+++ myplot.py
@@ -29,6 +29,49 @@ def plot_test_distrib(model, model_name, model_path, X_test, y_test):
         logger.warning(str(e))
 
 
+def plot_valid_distrib(model, model_name, model_path, X, y, classes=('b', 's')):
+    logger = logging.getLogger()
+    logger.info( 'Valid accuracy = {} %'.format(100 * model.score(X, y)) )
+    proba = model.predict_proba(X)
+    try:
+        sns.distplot(proba[y==0, 1], label=classes[0])
+        sns.distplot(proba[y==1, 1], label=classes[1])
+        plt.title(model_name)
+        plt.legend()
+        plt.savefig(os.path.join(model_path, 'valid_distrib.png'))
+        plt.clf()
+    except Exception as e:
+        logger.warning('Plot valid distrib failed')
+        logger.warning(str(e))
+
+
+def plot_summaries(summary_computer, model_name, model_path, 
+                    X_valid, y_valid, w_valid,
+                    X_test, w_test, classes=('b', 's', 'n') ):
+    logger = logging.getLogger()
+    X_sig = X_valid[y_valid==1]
+    w_sig = w_valid[y_valid==1]
+    X_bkg = X_valid[y_valid==0]
+    w_bkg = w_valid[y_valid==0]
+
+    s_histogram = summary_computer(X_sig, w_sig)
+    b_histogram = summary_computer(X_bkg, w_bkg)
+    n_histogram = summary_computer(X_test, w_test)
+
+    try:
+        plt.bar(np.arange(10)+0.1, b_histogram, width=0.3, label=classes[0])
+        plt.bar(np.arange(10)+0.4, s_histogram, width=0.3, label=classes[1])
+        plt.bar(np.arange(10)+0.7, n_histogram, width=0.3, label=classes[2])
+        plt.title(model_name)
+        plt.legend()
+        plt.savefig(os.path.join(model_path, 'summaries.png'))
+        plt.clf()
+    except Exception as e:
+        logger.warning('Plot summaries failed')
+        logger.warning(str(e))
+
+
+
 def plot_param_around_min(param_array, nll_array, true_value, param_name, model_path):
     logger = logging.getLogger()
     try:
