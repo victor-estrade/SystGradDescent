49c9b9cbe7cf69ee2898e0e6daa7a77417ed7bdf
Victor Estrade <victor.estrade@lri.fr>
2019-09-24 12:08:29 +0200

    Add negative log likelihood computing for benchmark AP1

--- problem/apples_and_pears.py
+++ problem/apples_and_pears.py
@@ -4,7 +4,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import numpy as np
-
+from .nll import gauss_nll
+from .nll import poisson_nll
 
 SEED = 42
 
@@ -68,3 +69,21 @@ class AP1():
         X, y, w = self.generator.generate(apple_ratio=apple_ratio, n_apple=n_apple, n_pear=n_pear)
         return X, y, w
 
+
+class AP1NLL():
+    def __init__(self, compute_summaries, valid_generator, X_exp, w_exp):
+        self.compute_summaries = compute_summaries
+        self.valid_generator = valid_generator
+        self.X_exp = X_exp
+        self.w_exp = w_exp
+    
+    def __call__(self, apple_ratio):
+        self.valid_generator.reset()
+        X, y, w = self.valid_generator.generate(apple_ratio, n_samples=2_000)
+        valid_summaries = self.compute_summaries(X, w)
+        test_summaries  = self.compute_summaries(self.X_exp, self.w_exp)
+        # Compute NLL
+        EPSILON = 1e-6  # avoid log(0)
+        rate = valid_summaries# + EPSILON
+        total_nll = np.sum(poisson_nll(test_summaries, rate))
+        return total_nll
