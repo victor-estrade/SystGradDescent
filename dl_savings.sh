e4970a2bb43ac1db06cd9c00f53eccabd5858541
Victor Estrade <victor.estrade@lri.fr>
2020-01-16 16:40:52 +0100

    Use rsync instead of scp

--- dl_savings.sh
+++ dl_savings.sh
@@ -1,2 +1,3 @@
-scp -r titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/savings/* ./savings
+rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/savings/ ./savings
 # scp -r titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/Playground.ipynb ./
+# scp titanic:/home/tao/vestrade/workspace/SystML/INFERNO/paper-inferno/code/*.ipynb ./
