193c4a7c1594cceabe822ef5d0a87980d3b846e5
Victor Estrade <victor.estrade@lri.fr>
2020-01-23 10:55:42 +0100

    Change S3D2 TRUE_MU_RANGE

--- problem/synthetic3D.py
+++ problem/synthetic3D.py
@@ -154,8 +154,8 @@ class S3D2Config():
     TRUE_R = 0.1
     TRUE_LAMBDA = 2.7
 
-    # TRUE_MU_RANGE = np.arange(0, 0.2, 0.05)
-    TRUE_MU_RANGE = [100/1050, 150/1050, 200/1050]
+    TRUE_MU_RANGE = np.arange(0, 0.3, 0.05)
+    # TRUE_MU_RANGE = [100/1050, 150/1050, 200/1050]
     TRUE_R_RANGE = np.arange(-0.2, 0.2, 0.1)
     TRUE_LAMBDA_RANGE = np.arange(2.1, 3.5, 0.2)
 
