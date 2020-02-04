c93f6a7073bf5c32a07929afb523ab63ef726811
Victor Estrade <victor.estrade@lri.fr>
2020-02-04 13:26:19 +0100

    Minor optimization of Average Layers. Looks like torch.nn.Linear source code

--- archi/layers.py
+++ archi/layers.py
@@ -24,62 +24,6 @@ def torch_weighted_sum(input, weight, dim, keepdim=False, dtype=None):
 # Pytorch Linear module implementation
 # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/linear.py#L33
 
-class AverageExtra(nn.Module):
-    def __init__(self, n_in, n_out, n_extra, bias=True):
-        super().__init__()
-        self.n_in = n_in
-        self.n_out = n_out
-        self.n_extra = n_extra
-        self.bias = bias
-        self.fc1 = nn.Linear(n_in, n_out, bias=False)
-        self.fc2 = nn.Linear(n_in+n_extra, n_out, bias=bias)
-        self.reset_parameters()
-
-    def forward(self, x, w, extra):
-        x_out = self.fc1(x)
-        x_mean = torch_weighted_mean(x, w, 0, keepdim=True)
-        x_mean = torch.cat((x_mean, extra), 1)
-        x_mean = self.fc2(x_mean)
-        out = x_mean + x_out
-        return out
-
-    def reset_parameters(self):
-        self.fc1.reset_parameters()
-        self.fc2.reset_parameters()
-
-    def extra_repr(self):
-        return 'in_features={}, out_features={}, extra_summaries={}, bias={}'.format(
-            self.n_in, self.n_out, self.n_extra, self.bias
-        )
-
-
-class Average(nn.Module):
-    def __init__(self, n_in, n_out, bias=True):
-        super().__init__()
-        self.n_in = n_in
-        self.n_out = n_out
-        self.bias = bias
-        self.fc1 = nn.Linear(n_in, n_out, bias=False)
-        self.fc2 = nn.Linear(n_in, n_out)
-        self.reset_parameters()
-
-    def forward(self, x, w):
-        x_out = self.fc1(x)
-        x_mean = torch_weighted_mean(x, w, 0, keepdim=True)
-        x_mean = self.fc2(x_mean)
-        out = x_mean + x_out
-        return out
-
-    def reset_parameters(self):
-        self.fc1.reset_parameters()
-        self.fc2.reset_parameters()
-
-    def extra_repr(self):
-        return 'in_features={}, out_features={}, bias={}'.format(
-            self.n_in, self.n_out, self.bias
-        )
-
-
 class SumExtra(nn.Module):
     def __init__(self, n_in, n_out, n_extra, bias=True):
         super().__init__()
@@ -134,7 +78,7 @@ class Sum(nn.Module):
         )
 
 
-class Average2(nn.Module):
+class Average(nn.Module):
 # Shamelessly copy and adapted from 
 # Pytorch Linear module implementation
 # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/linear.py#L33
@@ -174,4 +118,49 @@ class Average2(nn.Module):
     def extra_repr(self):
         return 'in_features={}, out_features={}, bias={}'.format(
             self.in_features, self.out_features, self.bias is not None
+        )
+
+
+class AverageExtra(nn.Module):
+# Shamelessly copy and adapted from 
+# Pytorch Linear module implementation
+# https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/linear.py#L33
+# Or permalink
+# https://github.com/pytorch/pytorch/blob/b894dc06de3e0750d9db8bd20b92429f6d873fa1/torch/nn/modules/linear.py#L33
+    __constants__ = ['in_features', 'out_features', 'extra_summaries']
+
+    def __init__(self, in_features, out_features, extra_summaries, bias=True):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.extra_summaries = extra_summaries
+        self.feature_weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.summary_weight = nn.Parameter(torch.Tensor(out_features, in_features+extra_summaries))
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.kaiming_uniform_(self.feature_weight, a=math.sqrt(5))
+        nn.init.kaiming_uniform_(self.summary_weight, a=math.sqrt(5))
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.summary_weight)
+            bound = 1 / math.sqrt(fan_in)
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input, weight, extra):
+        # x = F.linear(input, self.feature_weight, None)
+        x = input.matmul(self.feature_weight.t())
+        x_mean = torch_weighted_mean(input, weight, 0, keepdim=True)
+        x_mean = torch.cat((x_mean, extra), 1)
+        x_summary = F.linear(x_mean, self.summary_weight, self.bias)
+        output = x + x_summary
+        return output
+
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, extra_summaries={}, bias={}'.format(
+            self.in_features, self.out_features, self.extra_summaries, self.bias is not None
         )
