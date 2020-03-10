97445f05f69e0149fbdb44d66594eb31f2f55bd6
Victor Estrade <victor.estrade@lri.fr>
2020-03-10 18:00:10 +0100

    send local to cluster script

---
+++ send_savings.sh
@@ -0,0 +1,3 @@
+rsync -rtv ./savings/ titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/savings
+# scp -r titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/Playground.ipynb ./
+# scp titanic:/home/tao/vestrade/workspace/SystML/INFERNO/paper-inferno/code/*.ipynb ./
