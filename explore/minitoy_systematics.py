da523407b13195b79e525ff3219020a68ca219dc
Victor Estrade <victor.estrade@lri.fr>
2020-03-11 15:52:40 +0100

    init minitoy systematics exploring

---
+++ explore/minitoy_systematics.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+
+"""
+Exploring the possible minimal systematic error
+
+The idea is to build a very simple but representative toy and to compute statistic and systematic error.
+We can compute the likelihood for this mini toy.
+
+x = value of an observable of an event.
+y = target parameter = mixture coef of signal and background events
+alpha = nuisance parameter = rescaling of the observable
+
+"""
+
+
+def compute_proba_densitity(x, y, alpha):
+	pass
+	
+
+
+
+def main():
+	print('hello world !')
+
+
+if __name__ == '__main__':
+	main()
