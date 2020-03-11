bafb13ad034cdf9e051dff70db48faa430129d43
Victor Estrade <victor.estrade@lri.fr>
2020-03-11 18:35:31 +0100

    Turn function into classes to avoid reapeating contant definitions

--- explore/minitoy_systematics.py
+++ explore/minitoy_systematics.py
@@ -33,118 +33,141 @@ def assert_clean_y(y):
     assert y > 0 and y < 1, f"y is a mixture coef it should be in ]0, 1[  {y} found"
 
 
-def sample_event(y, alpha, size=1):
-    assert_clean_alpha(alpha)
-    assert_clean_y(y)
-    gamma_k      = 2
-    gamma_loc    = 0
-    gamma_scale  = alpha
-    normal_mean  = 5 * alpha
-    normal_sigma = 0.5 * alpha
-    x_b = sts.gamma.rvs(gamma_k, loc=gamma_loc, scale=gamma_scale, size=size)
-    x_s  = sts.norm.rvs(loc=normal_mean, scale=normal_sigma, size=size)
-    idx = np.random.random(size=size) < y
-    x_b[idx] = x_s[idx]
-    return x_b, idx
+class Generator():
+    def __init__(self, gamma_k=2, gamma_loc=0, normal_mean=5, normal_sigma=0.5):
+        self.gamma_k = gamma_k
+        self.gamma_loc = gamma_loc
+        self.normal_mean = normal_mean
+        self.normal_sigma = normal_sigma
+
+    def sample_event(self, y, alpha, size=1):
+        assert_clean_alpha(alpha)
+        assert_clean_y(y)
+        gamma_k      = self.gamma_k
+        gamma_loc    = self.gamma_loc
+        gamma_scale  = alpha
+        normal_mean  = self.normal_mean * alpha
+        normal_sigma = self.normal_sigma * alpha
+        x_b = sts.gamma.rvs(gamma_k, loc=gamma_loc, scale=gamma_scale, size=size)
+        x_s  = sts.norm.rvs(loc=normal_mean, scale=normal_sigma, size=size)
+        idx = np.random.random(size=size) < y
+        x_b[idx] = x_s[idx]
+        return x_b, idx
+
+    def proba_density(self, x, y, alpha):
+        """
+        computes p(x | y, alpha)
+        """
+        assert_clean_alpha(alpha)
+        assert_clean_y(y)
+        gamma_k      = self.gamma_k
+        gamma_loc    = self.gamma_loc
+        gamma_scale  = alpha
+        normal_mean  = self.normal_mean * alpha
+        normal_sigma = self.normal_sigma * alpha
+        proba_gamma  = sts.gamma.pdf(x, gamma_k, loc=gamma_loc, scale=gamma_scale)
+        proba_normal  = sts.norm.pdf(x, loc=normal_mean, scale=normal_sigma)
+        proba_density = y * proba_normal + (1-y) * proba_gamma
+        return proba_density
+
+    def log_proba_density(self, x, y, alpha):
+        """
+        computes log p(x | y, alpha)
+        """
+        proba_density = self.proba_density(x, y, alpha)
+        logproba_density = np.log(proba_density)
+        return logproba_density
+
+    def nll(self, data, y, alpha):
+        """
+        Computes the negative log likelihood of teh data given y and alpha.
+        """
+        nll = - self.log_proba_density(data, y, alpha).sum()
+        return nll
 
 
 
+class PriorAlpha():
+    def __init__(self, alpha_min=0.7, alpha_max=1.3):
+        self.alpha_min = alpha_min
+        self.alpha_max = alpha_max
 
-def compute_proba_densitity(x, y, alpha):
-    """
-    computes p(x | y, alpha)
-    """
-    assert_clean_alpha(alpha)
-    assert_clean_y(y)
-    gamma_k      = 2
-    gamma_loc    = 0
-    gamma_scale  = alpha
-    normal_mean  = 5 * alpha
-    normal_sigma = 0.5 * alpha
-    proba_gamma  = sts.gamma.pdf(x, gamma_k, loc=gamma_loc, scale=gamma_scale)
-    proba_normal  = sts.norm.pdf(x, loc=normal_mean, scale=normal_sigma)
-    proba_density = y * proba_normal + (1-y) * proba_gamma
-    return proba_density
+    def proba_density(self, alpha):
+        scale = self.alpha_max - self.alpha_min
+        p = sts.uniform.pdf(alpha, loc=self.alpha_min, scale=scale)
+        return p
+
+    def log_proba_density(self, alpha):
+        scale = self.alpha_max - self.alpha_min
+        p = sts.uniform.pdf(alpha, loc=self.alpha_min, scale=scale)
+        return p
 
 
-def compute_logproba_densitity(x, y, alpha):
-    """
-    computes log p(x | y, alpha)
-    """
-    proba_density = compute_proba_densitity(x, y, alpha)
-    logproba_density = np.log(proba_density)
-    return logproba_density
+class PriorY():
+    def __init__(self, y_min=0.01, y_max=0.3):
+        self.y_min = y_min
+        self.y_max = y_max
 
-def compute_prior_alpha(alpha):
-    alpha_min = 0.7
-    alpha_max = 1.3
-    scale = alpha_max - alpha_min
-    p = sts.uniform.pdf(alpha, loc=alpha_min, scale=scale)
-    return p
+    def proba_density(self, y):
+        scale = self.y_max - self.y_min
+        p = sts.uniform.pdf(y, loc=self.y_min, scale=scale)
+        return p
 
-def compute_logprior_alpha(alpha):
-    alpha_min = 0.7
-    alpha_max = 1.3
-    scale = alpha_max - alpha_min
-    p = sts.uniform.pdf(alpha, loc=alpha_min, scale=scale)
-    return p
+    def log_proba_density(self, y):
+        scale = self.y_max - self.y_min
+        p = sts.uniform.pdf(y, loc=self.y_min, scale=scale)
+        return p
 
 
-def compute_prior_y(y):
-    y_min = 0.01
-    y_max = 0.3
-    scale = y_max - y_min
-    p = sts.uniform.pdf(y, loc=y_min, scale=scale)
-    return p
-
-
-def compute_nll(data, y, alpha):
-    nll = - sum([compute_logproba_densitity(x, y, alpha) for x in data])
-    return nll
-
-
-def compute_posterior
 
 
 def main():
     print('hello world !')
     x_range = np.linspace(0, 10, 1000)
+    generator = Generator()
+    prior_alpha = PriorAlpha()
+    prior_y = PriorY()
     alpha =  1
     y = 0.5
-    p = compute_proba_densitity(x_range, y, alpha)
+    p = generator.proba_density(x_range, y, alpha)
     plt.plot(x_range, p, label=f"{alpha}")
-    data, idx = sample_event(y, alpha, size=1000)
+    data, idx = generator.sample_event(y, alpha, size=1000)
     sns.distplot(data, label="samples")
 
     alpha = 1.1
-    p = compute_proba_densitity(x_range, y, alpha)
+    p = generator.proba_density(x_range, y, alpha)
     plt.plot(x_range, p, label=f"{alpha}")
     plt.legend()
     plt.show()
 
     x_range = np.linspace(0, 2, 100)
-    p = compute_prior_alpha(x_range)
+    p = prior_alpha.proba_density(x_range)
     plt.plot(x_range, p, label=f"p_alpha")
     plt.legend()
     plt.show()
 
 
     x_range = np.linspace(0, 0.5, 100)
-    p = compute_prior_y(x_range)
+    p = prior_y.proba_density(x_range)
     plt.plot(x_range, p, label=f"p_y")
     plt.legend()
     plt.show()
 
     print('NLL arange 0, 10' )
-    nll = compute_nll(data, 0.1, 1.1)
+
+    nll = generator.nll(data, 0.1, 1.1)
     print('nll =', nll )
-    nll = compute_nll(data, 0.01, 1.1)
+    
+    nll = generator.nll(data, 0.01, 1.1)
     print('nll =', nll )
-    nll = compute_nll(data, 0.5, 1.1)
+    
+    nll = generator.nll(data, 0.5, 1.1)
     print('nll =', nll )
-    nll = compute_nll(data, 0.45, 1)
+    
+    nll = generator.nll(data, 0.45, 1)
     print('nll =', nll )
-    nll = compute_nll(data, 0.5, 1)
+    
+    nll = generator.nll(data, 0.5, 1)
     print('best nll =', nll )
 
 
