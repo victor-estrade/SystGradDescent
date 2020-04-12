622dc94f23201023bdd5924e518318426b6736a5
Victor Estrade <victor.estrade@lri.fr>
2020-04-12 16:23:00 +0200

    Example of main using new help methods

--- benchmark/S3D2/GB.py
+++ benchmark/S3D2/GB.py
@@ -16,37 +16,35 @@ ERRORDEF_NLL = 0.5
 
 import pandas as pd
 
-from utils.plot import set_plot_config
+from visual.misc import set_plot_config
 set_plot_config()
+
 from utils.log import set_logger
 from utils.log import flush
 from utils.log import print_line
-from utils.log import print_params
 from utils.model import get_model
-from utils.model import save_model
-from utils.misc import gather_images
-from utils.misc import estimate
-from utils.misc import register_params
-from utils.misc import evaluate_estimator
+from utils.model import train_or_load_classifier
+from utils.evaluation import evaluate_classifier
+from utils.evaluation import evaluate_summary_computer
+from utils.evaluation import evaluate_minuit
+from utils.evaluation import estimate
+from utils.evaluation import evaluate_estimator
+from utils.images import gather_images
 
-from utils.plot import plot_valid_distrib
-from utils.plot import plot_summaries
-from utils.plot import plot_params
+from visual.misc import plot_params
 
 from problem.synthetic3D import S3D2Config
 from problem.synthetic3D import S3D2
 from problem.synthetic3D import S3D2NLL
 
-from utils.S3D2 import plot_R_around_min
-from utils.S3D2 import plot_LAMBDA_around_min
-from utils.S3D2 import plot_MU_around_min
+from visual.S3D2 import plot_nll_around_min
 
 from model.gradient_boost import GradientBoostingModel
 from model.summaries import ClassifierSummaryComputer
 from ..my_argparser import GB_parse_args
 
 
-BENCHMARK_NAME = 'S3D2'
+BENCHMARK_NAME = 'S3D2-prior'
 N_ITER = 9
 
 
@@ -95,7 +93,6 @@ def run(args, i_cv):
     valid_generator = S3D2(seed+1)
     test_generator  = S3D2(seed+2)
 
-
     # SET MODEL
     logger.info('Set up classifier')
     model = get_model(args, GradientBoostingModel)
@@ -103,27 +100,12 @@ def run(args, i_cv):
     flush(logger)
     
     # TRAINING / LOADING
-    if not args.retrain:
-        try:
-            logger.info('loading from {}'.format(model.path))
-            model.load(model.path)
-        except Exception as e:
-            logger.warning(e)
-            args.retrain = True
-    if args.retrain:
-        logger.info('Generate training data')
-        X_train, y_train, w_train = train_generator.generate(
-                                         pb_config.CALIBRATED_R,
-                                         pb_config.CALIBRATED_LAMBDA,
-                                         pb_config.CALIBRATED_MU,
-                                         n_samples=pb_config.N_TRAINING_SAMPLES)
-        logger.info('Training {}'.format(model.get_name()))
-        model.fit(X_train, y_train, w_train)
-        logger.info('Training DONE')
-
-        # SAVE MODEL
-        save_model(model)
-
+    X_train, y_train, w_train = train_generator.generate(
+                                     pb_config.CALIBRATED_R,
+                                     pb_config.CALIBRATED_LAMBDA,
+                                     pb_config.CALIBRATED_MU,
+                                     n_samples=pb_config.N_TRAINING_SAMPLES)
+    train_or_load_classifier(model, X_train, y_train, w_train, retrain=args.retrain)
 
     # CHECK TRAINING
     logger.info('Generate validation data')
@@ -133,14 +115,11 @@ def run(args, i_cv):
                                      pb_config.CALIBRATED_MU,
                                      n_samples=pb_config.N_VALIDATION_SAMPLES)
     
-    logger.info('Plot distribution of the score')
-    plot_valid_distrib(model, X_valid, y_valid, classes=("b", "s"))
-    result_row['valid_accuracy'] = model.score(X_valid, y_valid)
-
+    result_row.update(evaluate_classifier(model, X_valid, y_valid, w_valid, prefix='valid'))
 
     # MEASUREMENT
-    n_bins = 10
-    compute_summaries = ClassifierSummaryComputer(model, n_bins=n_bins)
+    N_BINS = 10
+    compute_summaries = ClassifierSummaryComputer(model, n_bins=N_BINS)
     for mu in pb_config.TRUE_MU_RANGE:
         pb_config.TRUE_MU = mu
         logger.info('Generate testing data')
@@ -153,30 +132,21 @@ def run(args, i_cv):
         logger.info('Set up NLL computer')
         compute_nll = S3D2NLL(compute_summaries, valid_generator, X_test, w_test)
 
-        logger.info('Plot summaries')
-        extension = '-mu={:1.2f}_r={}_lambda={}'.format(pb_config.TRUE_MU, 
+        suffix = '-mu={:1.2f}_r={}_lambda={}'.format(pb_config.TRUE_MU,
                                     pb_config.TRUE_R, pb_config.TRUE_LAMBDA)
-        plot_summaries( model, n_bins, extension,
-                        X_valid, y_valid, w_valid,
-                        X_test, w_test, classes=('b', 's', 'n') )
 
+        evaluate_summary_computer(model, X_valid, y_valid, w_valid, X_test, w_test, n_bins=N_BINS, prefix='', suffix=suffix)
 
         # NLL PLOTS
-        logger.info('Plot NLL around minimum')
-        plot_R_around_min(compute_nll, pb_config, model, extension)
-        plot_LAMBDA_around_min(compute_nll, pb_config, model, extension)
-        plot_MU_around_min(compute_nll, pb_config, model, extension)
+        plot_nll_around_min(compute_nll, pb_config, model.path, suffix)
 
         # MINIMIZE NLL
         logger.info('Prepare minuit minimizer')
         minimizer = get_minimizer(compute_nll, pb_config)
         fmin, params = estimate(minimizer)
         params_truth = [pb_config.TRUE_R, pb_config.TRUE_LAMBDA, pb_config.TRUE_MU]
+        result_row.update(evaluate_minuit(minimizer, fmin, params, params_truth))
 
-        print_params(params, params_truth)
-        register_params(params, params_truth, result_row)
-        result_row['is_mingrad_valid'] = minimizer.migrad_ok()
-        result_row.update(fmin)
         result_table.append(result_row.copy())
     result_table = pd.DataFrame(result_table)
     logger.info('Plot params')
