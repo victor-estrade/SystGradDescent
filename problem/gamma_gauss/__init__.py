5590f60f0390f0724647e19dd8e1b23f905db27a
Victor Estrade <victor.estrade@lri.fr>
2020-04-12 19:14:03 +0200

    init gamma gauss problem

---
+++ problem/gamma_gauss/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+# from .generator import S3D2
+# from .parameter import Parameter
+# from .nll import S3D2NLL
+# from .config import S3D2Config
+# from .calibration import param_generator
+
+
