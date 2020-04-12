011bf2e5bf15d629a4e9ad223509c9256790f2b0
Victor Estrade <victor.estrade@lri.fr>
2020-04-12 15:40:16 +0200

    refactor plot_summaries

--- visual/likelihood.py
+++ visual/likelihood.py
@@ -16,30 +16,22 @@ from config import DEFAULT_DIR
 
 
 
-def plot_summaries(model, n_bins, extension,
-                    X_valid, y_valid, w_valid,
-                    X_test, w_test, classes=('b', 's', 'n') ):
+def plot_summaries(b_histogram, s_histogram, n_histogram, 
+                    title='no title', directory=DEFAULT_DIR, fname='summaries.png', 
+                    classes=('b', 's', 'n'),):
     logger = logging.getLogger()
-    X_sig = X_valid[y_valid==1]
-    w_sig = w_valid[y_valid==1]
-    X_bkg = X_valid[y_valid==0]
-    w_bkg = w_valid[y_valid==0]
-
-    s_histogram = model.compute_summaries(X_sig, w_sig, n_bins)
-    b_histogram = model.compute_summaries(X_bkg, w_bkg, n_bins)
-    n_histogram = model.compute_summaries(X_test, w_test, n_bins)
-
+    n_bins = len(b_histogram)
+    x_ticks = np.arange(n_bins)
     try:
-        x_ticks = np.arange(n_bins)
         plt.bar(x_ticks+0.1, b_histogram, width=0.3, label=classes[0])
         plt.bar(x_ticks+0.4, s_histogram, width=0.3, label=classes[1])
         plt.bar(x_ticks+0.7, n_histogram, width=0.3, label=classes[2])
         plt.xlabel("bins")
         plt.ylabel("summary_value")
         plt.xticks(x_ticks)
-        plt.title(model.full_name)
+        plt.title()
         plt.legend()
-        plt.savefig(os.path.join(model.path, 'summaries{}.png'.format(extension)))
+        plt.savefig(os.path.join(directory, fname))
         plt.clf()
     except Exception as e:
         logger.warning('Plot summaries failed')
