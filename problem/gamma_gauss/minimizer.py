f2649f58da19bafe427e7e77db982fa05784c0de
Victor Estrade <victor.estrade@lri.fr>
2020-04-15 19:15:21 +0200

    Fix parameter name

--- problem/gamma_gauss/minimizer.py
+++ problem/gamma_gauss/minimizer.py
@@ -10,9 +10,9 @@ ERRORDEF_NLL = 0.5
 def get_minimizer(compute_nll, calibrated_param, calibrated_param_error):
     minimizer = iminuit.Minuit(compute_nll,
                            errordef=ERRORDEF_NLL,
-                           alpha=calibrated_param.alpha,
-                           error_alpha=calibrated_param_error.alpha,
-                           limit_alpha=(0, None),
+                           rescale=calibrated_param.rescale,
+                           error_rescale=calibrated_param_error.rescale,
+                           limit_rescale=(0, None),
                            mix=calibrated_param.mix,
                            error_mix=calibrated_param_error.mix,
                            limit_mix=(0, 1),
