9ee6e3caebe46b3801442ae0eda89407a35afa6d
Victor Estrade <victor.estrade@lri.fr>
2020-04-16 21:01:11 +0200

    reduced RANGE for fast test

--- problem/gamma_gauss/config.py
+++ problem/gamma_gauss/config.py
@@ -10,8 +10,8 @@ class GGConfig():
     CALIBRATED = Parameter(rescale=1, mix=0.1)
     CALIBRATED_ERROR = Parameter(rescale=2, mix=1)
     TRUE = Parameter(rescale=1.2, mix=0.2)
-    RANGE = Parameter(rescale=np.linspace(0.5, 3, 20), 
-                        mix=np.linspace(0.1, 0.9, 10))
+    RANGE = Parameter(rescale=np.linspace(0.8, 2, 4), 
+                        mix=np.linspace(0.1, 0.9, 4))
 
     PARAM_NAMES = TRUE._fields
 
