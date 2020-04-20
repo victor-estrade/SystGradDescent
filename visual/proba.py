56a839fd800d5f03e5fe2b0356b4a0a359b3b97d
Victor Estrade <victor.estrade@lri.fr>
2020-04-20 16:49:42 +0200

    add plot_distrib in visual.special.gamma_gauss

---
+++ visual/proba.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+import logging
+
+import matplotlib.pyplot as plt
+
+from config import DEFAULT_DIR
+
+
+def plot_infer(values, probas, expected_value=None, true_value=None, std=None,
+                title="posterior marginal proba", directory=DEFAULT_DIR, fname='marginal.png', 
+                name=''):
+    logger = logging.getLogger()
+
+    try:
+        if true_value is not None:
+            plt.axvline(true_value, c="orange", label=f"true {name}")
+            if std is not None:
+                plt.axvline(true_value-std, c="orange", label=f"true {name} - std({name})")
+                plt.axvline(true_value+std, c="orange", label=f"true {name} + std({name})")
+        if expected_value is not None:
+            plt.axvline(expected_value, c="green", label=f"E[{name}|x]")
+        plt.plot(values, probas, label="posterior")
+        plt.xlabel(f"{name} values")
+        plt.ylabel("proba density")
+        plt.title(title)
+        plt.legend()
+        plt.savefig(os.path.join(directory, fname))
+        plt.clf()
+    except Exception as e:
+        logger.warning(f'Plot infer {name} failed')
+        logger.warning(str(e))
--- visual/special/gamma_gauss.py
+++ visual/special/gamma_gauss.py
@@ -5,7 +5,13 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import logging
+import os
 import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from config import DEFAULT_DIR
 
 from ..likelihood import plot_param_around_min
 
@@ -26,3 +32,25 @@ def plot_nll_around_min(compute_nll, true_params, directory, suffix):
     logger.info('Plot NLL around minimum')
     plot_RESCALE_around_min(compute_nll, true_params, directory, suffix)
     plot_MIX_around_min(compute_nll, true_params, directory, suffix)
+
+
+def plot_distrib(data, generator, true_params, expect_rescale, expect_mix,
+                title="data distribution", directory=DEFAULT_DIR, fname='data_distrib.png'):
+    logger = logging.getLogger()
+    x_range = np.linspace(np.min(data), np.max(data), 1000)
+    true_proba = generator.proba_density(x_range, *true_params)
+    infered_proba = generator.proba_density(x_range, expect_rescale, expect_mix)
+
+    try:
+        sns.distplot(data, label="data hist")
+        plt.plot(x_range, true_proba, label="true proba")
+        plt.plot(x_range, infered_proba, '--', label="infered proba")
+        plt.title(title)
+        plt.xlabel("x")
+        plt.ylabel("density")
+        plt.legend()
+        plt.savefig(os.path.join(directory, 'data_distrib.png'))
+        plt.clf()
+    except Exception as e:
+        logger.warning(f'Plot distrib failed')
+        logger.warning(str(e))
