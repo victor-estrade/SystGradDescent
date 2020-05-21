694114f129fbbbb16fb08be473a154b36f360638
Victor Estrade <victor.estrade@lri.fr>
2020-05-21 18:44:05 +0200

    fill init

--- problem/higgs/__init__.py
+++ problem/higgs/__init__.py
@@ -3,3 +3,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
+from .generator import Generator
+from .generator import get_generators
+from .parameter import Parameter
+from .nll import HiggsNLL
+from .config import HiggsConfig
+from .calibration import param_generator
+from .minimizer import get_minimizer
