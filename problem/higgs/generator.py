c42312d17b78f8b4baa11c67c7e66bc52e181243
Victor Estrade <victor.estrade@lri.fr>
2020-05-21 18:46:20 +0200

    Fix data split and missing mu in generate()

--- problem/higgs/generator.py
+++ problem/higgs/generator.py
@@ -11,30 +11,31 @@ from .higgs_geant import split_data_label_weights
 from .higgs_geant import load_data
 from .higgs_geant import normalize_weight
 from .higgs_4v_pandas import mu_reweighting
-from .higgs_4v_pandas import tau_energy_scale
-from .higgs_4v_pandas import jet_energy_scale
-from .higgs_4v_pandas import lep_energy_scale
-from .higgs_4v_pandas import soft_term
+from .higgs_4v_pandas import syst_effect
 from .higgs_4v_pandas import nasty_background
 
 from sklearn.model_selection import ShuffleSplit
 
 
-def get_generators(seed):
+def get_generators(seed, train_size=0.5, test_size=0.1):
     data = load_data()
+    # print("data.shape", data.shape)
     data['origWeight'] = data['Weight'].copy()
-    cv_train_other = ShuffleSplit(n_splits=1, test_size=0.5, random_state=seed)
+    cv_train_other = ShuffleSplit(n_splits=1, train_size=train_size, test_size=1-train_size, random_state=seed)
     idx_train, idx_other = next(cv_train_other.split(data, data['Label']))
     train_data = data.iloc[idx_train]
     train_generator = Generator(train_data, seed=seed)
     other_data = data.iloc[idx_other]
+    # print("train_data.shape", train_data.shape)
 
-    cv_valid_test = ShuffleSplit(n_splits=1, test_size=0.5, random_state=seed+1)
+    cv_valid_test = ShuffleSplit(n_splits=1, test_size=test_size, random_state=seed+1)
     idx_valid, idx_test = next(cv_valid_test.split(other_data, other_data['Label']))
     valid_data = other_data.iloc[idx_valid]
     test_data = other_data.iloc[idx_test]
     valid_generator = Generator(valid_data, seed=seed+1)
     test_generator = Generator(test_data, seed=seed+2)
+    # print("valid_data.shape", valid_data.shape)
+    # print("test_data.shape", test_data.shape)
 
     return train_generator, valid_generator, test_generator
 
@@ -77,15 +78,12 @@ class Generator():
         return data_sample
 
 
-    def generate(self, mu, tau_es, jet_es, lep_es, sigma_soft, nasty_bkg, n_samples=None):
+    def generate(self, tau_es, jet_es, lep_es, nasty_bkg, sigma_soft, mu, n_samples=None):
         if n_samples is None:
             data = self.data.copy()
         else:
             data = self.sample(n_samples).copy()
-        tau_energy_scale(data, scale=tau_es)
-        jet_energy_scale(data, scale=jet_es)
-        lep_energy_scale(data, scale=lep_es)
-        soft_term(data, sigma_soft)
+        syst_effect(data, tes=tau_es, jes=jet_es, les=lep_es, sigma_met=sigma_soft, missing_value=0.0)
         normalize_weight(data)
         nasty_background(data, nasty_bkg)
         mu_reweighting(data, mu)
