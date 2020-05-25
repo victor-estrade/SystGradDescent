945b84b258929d21d1a40c46abc4542363dd0d9f
Victor Estrade <victor.estrade@lri.fr>
2020-05-25 11:59:34 +0200

    add GG.CALIB-Resclae

---
+++ benchmark/GG/CALIB-Rescale.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+# Command line : 
+# python -m benchmark.S3D2.CALIB-R
+
+import os
+import logging
+from config import SEED
+
+import pandas as pd
+
+from utils.log import set_logger
+from utils.log import flush
+from utils.log import print_line
+from utils.evaluation import evaluate_neural_net
+from utils.evaluation import evaluate_regressor
+from utils.evaluation import evaluate_estimator
+from utils.images import gather_images
+from utils.model import get_model
+from utils.model import get_optimizer
+from utils.model import save_model
+
+from config import _ERROR
+from config import _TRUTH
+
+from visual.misc import plot_params
+
+from problem.gamma_gauss import GGConfig as Config
+from problem.gamma_gauss import Generator
+from problem.gamma_gauss import param_generator
+
+from model.regressor import Regressor
+
+from archi.reducer import A3ML3 as ARCHI
+
+from ..my_argparser import REG_parse_args
+
+BENCHMARK_NAME = 'GG'
+CALIB = "Calib_rescale"
+
+
+class TainGenerator:
+    def __init__(self, param_generator, data_generator):
+        self.param_generator = param_generator
+        self.data_generator = data_generator
+
+    def generate(self, n_samples):
+        params = self.param_generator()
+        X, y, w = self.data_generator.generate(*params, n_samples)
+        return X, params.rescale, w, None
+
+
+def main():
+    # BASIC SETUP
+    logger = set_logger()
+    args = REG_parse_args(main_description="Training launcher for Regressor on S3D2 benchmark")
+    logger.info(args)
+    flush(logger)
+
+    # Setup model
+    logger.info("Setup model")
+    args.net = ARCHI(n_in=1, n_out=2, n_unit=args.n_unit)
+    args.optimizer = get_optimizer(args)
+    model = get_model(args, Regressor)
+    model.base_name = CALIB
+    model.set_info(BENCHMARK_NAME, 0)
+
+    # Setup data
+    logger.info("Setup data")
+    config = Config()
+    seed = SEED + 99999
+    train_generator = TainGenerator(param_generator, Generator(seed))
+    valid_generator = Generator(seed+1)
+    test_generator  = Generator(seed+2)
+
+    i_cv = -1
+    result_row = {'i_cv': i_cv}
+
+    # TRAIN / LOAD
+    if not args.retrain:
+        try:
+            logger.info('loading from {}'.format(model.path))
+            model.load(model.path)
+        except Exception as e:
+            logger.warning(e)
+            args.retrain = True
+    if args.retrain:
+        logger.info('Training {}'.format(model.get_name()))
+        model.fit(train_generator)
+        logger.info('Training DONE')
+        # SAVE
+        save_model(model)
+    flush(logger)
+
+    # CHECK TRAINING
+    result_row.update(evaluate_neural_net(model, prefix='valid'))
+    print_line()
+
+
+    result_table = [run_iter(model, result_row, i, test_config, valid_generator, test_generator)
+                    for i, test_config in enumerate(config.iter_test_config())]
+    result_table = pd.DataFrame(result_table)
+    result_table.to_csv(os.path.join(model.path, 'results.csv'))
+
+    logger.info('Plot params')
+    param_names = ["r"]
+    for name in param_names:
+        plot_params(name, result_table, title=model.full_name, directory=model.path)
+
+    logger.info('DONE')
+
+
+def run_iter(model, result_row, i_iter, config, valid_generator, test_generator):
+    logger = logging.getLogger()
+    logger.info('-'*45)
+    logger.info(f'iter : {i_iter}')
+    X_test, y_test, w_test = test_generator.generate(*config.TRUE, n_samples=config.N_TESTING_SAMPLES)
+    target, sigma = model.predict(X_test, w_test)
+    logger.info('{} =vs= {} +/- {}'.format(config.TRUE.rescale, target, sigma))
+
+    name = "r"
+    result_row[name] = target
+    result_row[name+_ERROR] = sigma
+    result_row[name+_TRUTH] = config.TRUE.r
+    return result_row.copy()
+
+
+if __name__ == '__main__':
+    main()
