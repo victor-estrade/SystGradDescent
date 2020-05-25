808e594c99171225c16066f04d73cd85aa104668
Victor Estrade <victor.estrade@lri.fr>
2020-05-25 11:59:03 +0200

    Add GG.GB-Prior

---
+++ benchmark/GG/GB-Prior.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+# Command line : 
+# python -m benchmark.GG.GB
+
+import os
+import logging
+from config import SEED
+
+import pandas as pd
+
+from visual.misc import set_plot_config
+set_plot_config()
+
+from utils.log import set_logger
+from utils.log import flush
+from utils.log import print_line
+from utils.model import get_model
+from utils.model import train_or_load_classifier
+from utils.evaluation import evaluate_classifier
+from utils.evaluation import evaluate_summary_computer
+from utils.evaluation import evaluate_minuit
+from utils.evaluation import evaluate_estimator
+from utils.images import gather_images
+
+from visual.misc import plot_params
+
+from problem.gamma_gauss import GGConfig as Config
+from problem.gamma_gauss import get_minimizer
+from problem.gamma_gauss import Generator
+from problem.gamma_gauss import GGNLL as NLLComputer
+
+from visual.special.gamma_gauss import plot_nll_around_min
+
+from model.gradient_boost import GradientBoostingModel
+from model.summaries import ClassifierSummaryComputer
+from ..my_argparser import GB_parse_args
+
+
+BENCHMARK_NAME = 'GammaGauss-prior'
+N_ITER = 3
+
+
+# =====================================================================
+# MAIN
+# =====================================================================
+def main():
+    # BASIC SETUP
+    logger = set_logger()
+    args = GB_parse_args(main_description="Training launcher for Gradient boosting on S3D2 benchmark")
+    logger.info(args)
+    flush(logger)
+    # INFO
+    model = get_model(args, GradientBoostingModel)
+    model.set_info(BENCHMARK_NAME, -1)
+    config = Config()
+    # RUN
+    results = [run(args, i_cv) for i_cv in range(N_ITER)]
+    results = pd.concat(results, ignore_index=True)
+    results.to_csv(os.path.join(model.directory, 'results.csv'))
+    # EVALUATION
+    eval_table = evaluate_estimator(config.INTEREST_PARAM_NAME, results)
+    print_line()
+    print_line()
+    print(eval_table)
+    print_line()
+    print_line()
+    eval_table.to_csv(os.path.join(model.directory, 'evaluation.csv'))
+    gather_images(model.directory)
+
+
+def run(args, i_cv):
+    logger = logging.getLogger()
+    print_line()
+    logger.info('Running iter n°{}'.format(i_cv))
+    print_line()
+
+    result_row = {'i_cv': i_cv}
+
+    # LOAD/GENERATE DATA
+    logger.info('Set up data generator')
+    config = Config()
+    seed = SEED + i_cv * 5
+    train_generator = Generator(seed)
+    valid_generator = Generator(seed+1)
+    test_generator  = Generator(seed+2)
+
+    # SET MODEL
+    logger.info('Set up classifier')
+    model = get_model(args, GradientBoostingModel)
+    model.set_info(BENCHMARK_NAME, i_cv)
+    flush(logger)
+    
+    # TRAINING / LOADING
+    train_or_load_classifier(model, train_generator, config.CALIBRATED, config.N_TRAINING_SAMPLES, retrain=args.retrain)
+
+    # CHECK TRAINING
+    logger.info('Generate validation data')
+    X_valid, y_valid, w_valid = valid_generator.generate(*config.CALIBRATED, n_samples=config.N_VALIDATION_SAMPLES)
+    
+    result_row.update(evaluate_classifier(model, X_valid, y_valid, w_valid, prefix='valid'))
+
+    # MEASUREMENT
+    N_BINS = 10
+    evaluate_summary_computer(model, X_valid, y_valid, w_valid, n_bins=N_BINS, prefix='valid_', suffix='')
+    result_table = [run_iter(model, result_row, i, test_config, valid_generator, test_generator, n_bins=10)
+                    for i, test_config in enumerate(config.iter_test_config())]
+    result_table = pd.DataFrame(result_table)
+    result_table.to_csv(os.path.join(model.path, 'results.csv'))
+    logger.info('Plot params')
+    param_names = config.PARAM_NAMES
+    for name in param_names:
+        plot_params(name, result_table, title=model.full_name, directory=model.path)
+
+    logger.info('DONE')
+    return result_table
+
+
+def run_iter(model, result_row, i_iter, config, valid_generator, test_generator, n_bins=10):
+    logger = logging.getLogger()
+    iter_directory = os.path.join(model.path, f'iter_{i_iter}')
+    os.makedirs(iter_directory, exist_ok=True)
+    result_row['i'] = i_iter
+    suffix = f'-mix={config.TRUE.mix:1.2f}_rescale={config.TRUE.rescale}'
+    logger.info('Generate testing data')
+    X_test, y_test, w_test = test_generator.generate(*config.TRUE, n_samples=config.N_TESTING_SAMPLES)
+    # PLOT SUMMARIES
+    evaluate_summary_computer(model, X_test, y_test, w_test, n_bins=n_bins, prefix='', suffix=suffix)
+
+    logger.info('Set up NLL computer')
+    compute_summaries = ClassifierSummaryComputer(model, n_bins=n_bins)
+    compute_nll = NLLComputer(compute_summaries, valid_generator, X_test, w_test, config=config)
+    # NLL PLOTS
+    plot_nll_around_min(compute_nll, config.TRUE, iter_directory, suffix)
+
+    # MINIMIZE NLL
+    logger.info('Prepare minuit minimizer')
+    minimizer = get_minimizer(compute_nll, config.CALIBRATED, config.CALIBRATED_ERROR)
+    result_row.update(evaluate_minuit(minimizer, config.TRUE))
+    return result_row.copy()
+
+if __name__ == '__main__':
+    main()
