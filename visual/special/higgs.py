d81482da25220385f27be17cc044a0b70fe1f42f
Victor Estrade <victor.estrade@lri.fr>
2020-06-18 16:48:55 +0200

    Adapt to 3 nuisance param higgs visuals

--- visual/special/higgs.py
+++ visual/special/higgs.py
@@ -17,22 +17,28 @@ from ..likelihood import plot_param_around_min
 
 def plot_TES_around_min(compute_nll, true_params, directory, suffix):
     tes_array = np.linspace(0.8, 1.2, 20)  # TODO : choose good range
-    tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
-    nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for tes in tes_array]
+    # tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
+    # nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for tes in tes_array]
+    tes, jes, les, mu =  true_params
+    nll_array = [compute_nll(tes, jes, les, mu) for tes in tes_array]
     name = 'tes'
     plot_param_around_min(tes_array, nll_array, true_params.tes, name, suffix, directory, 'tes NLL')
 
 def plot_JES_around_min(compute_nll, true_params, directory, suffix):
     jes_array = np.linspace(0.8, 1.2, 20)  # TODO : choose good range
-    tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
-    nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for jes in jes_array]
+    # tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
+    # nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for jes in jes_array]
+    tes, jes, les, mu =  true_params
+    nll_array = [compute_nll(tes, jes, les, mu) for jes in jes_array]
     name = 'jes'
     plot_param_around_min(jes_array, nll_array, true_params.jes, name, suffix, directory, 'jes NLL')
 
 def plot_LES_around_min(compute_nll, true_params, directory, suffix):
     les_array = np.linspace(0.9, 1.1, 20)  # TODO : choose good range
-    tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
-    nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for les in les_array]
+    # tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
+    # nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for les in les_array]
+    tes, jes, les, mu =  true_params
+    nll_array = [compute_nll(tes, jes, les, mu) for les in les_array]
     name = 'les'
     plot_param_around_min(les_array, nll_array, true_params.les, name, suffix, directory, 'les NLL')
 
@@ -55,8 +61,10 @@ def plot_SIGMA_SOFT_around_min(compute_nll, true_params, directory, suffix):
 
 def plot_MU_around_min(compute_nll, true_params, directory, suffix):
     mu_array = np.linspace(0.1, 3, 20)  # TODO : choose good range
-    tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
-    nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for mu in mu_array]
+    # tes, jes, les, nasty_bkg, sigma_soft, mu =  true_params
+    # nll_array = [compute_nll(tes, jes, les, nasty_bkg, sigma_soft, mu) for mu in mu_array]
+    tes, jes, les, mu =  true_params
+    nll_array = [compute_nll(tes, jes, les, mu) for mu in mu_array]
     name = 'mu'
     plot_param_around_min(mu_array, nll_array, true_params.mu, name, suffix, directory, 'mu NLL')
 
@@ -67,7 +75,8 @@ def plot_nll_around_min(compute_nll, true_params, directory, suffix):
     plot_TES_around_min(compute_nll, true_params, directory, suffix)
     plot_JES_around_min(compute_nll, true_params, directory, suffix)
     plot_LES_around_min(compute_nll, true_params, directory, suffix)
-    plot_NASTY_BKG_around_min(compute_nll, true_params, directory, suffix)
+    # TODO : FUTURE Reactivate
+    # plot_NASTY_BKG_around_min(compute_nll, true_params, directory, suffix)
     # plot_SIGMA_SOFT_around_min(compute_nll, true_params, directory, suffix)
     plot_MU_around_min(compute_nll, true_params, directory, suffix)
 
