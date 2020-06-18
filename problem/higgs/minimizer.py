606ce9e3075ab602df51c37f49a2f69b37a066db
Victor Estrade <victor.estrade@lri.fr>
2020-06-18 16:51:31 +0200

    get_minimizer to 3 nuisance params and future_minimizer for 5

--- problem/higgs/minimizer.py
+++ problem/higgs/minimizer.py
@@ -20,6 +20,27 @@ def get_minimizer(compute_nll, calibrated_param, calibrated_param_error):
                            les=calibrated_param.les,
                            error_les=calibrated_param_error.les,
                            limit_les=(MIN_VALUE, MAX_VALUE),
+                           mu=calibrated_param.mu,
+                           error_mu=calibrated_param_error.mu,
+                           limit_mu=(MIN_VALUE, 5),
+                          )
+    return minimizer
+
+
+def futur_get_minimizer(compute_nll, calibrated_param, calibrated_param_error):
+    MIN_VALUE = 0.01
+    MAX_VALUE = 10 
+    minimizer = iminuit.Minuit(compute_nll,
+                           errordef=ERRORDEF_NLL,
+                           tes=calibrated_param.tes,
+                           error_tes=calibrated_param_error.tes,
+                           limit_tes=(MIN_VALUE, MAX_VALUE),
+                           jes=calibrated_param.jes,
+                           error_jes=calibrated_param_error.jes,
+                           limit_jes=(MIN_VALUE, MAX_VALUE),
+                           les=calibrated_param.les,
+                           error_les=calibrated_param_error.les,
+                           limit_les=(MIN_VALUE, MAX_VALUE),
                            nasty_bkg=calibrated_param.nasty_bkg,
                            error_nasty_bkg=calibrated_param_error.nasty_bkg,
                            limit_nasty_bkg=(MIN_VALUE, MAX_VALUE),
