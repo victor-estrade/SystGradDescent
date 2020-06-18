a1f94dd20e06755394e88e2a91561fbfbaa0f56e
Victor Estrade <victor.estrade@lri.fr>
2020-06-18 16:52:42 +0200

    Add benchmark.HIGGS.likelihood_label

---
+++ benchmark/HIGGS/likelihood_label.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+import logging
+
+import numpy as np
+import pandas as pd
+
+from config import SAVING_DIR
+from config import SEED
+
+from visual import set_plot_config
+set_plot_config()
+from visual.misc import plot_params
+
+from utils.log import set_logger
+from utils.log import print_line
+from utils.evaluation import evaluate_minuit
+from utils.evaluation import evaluate_estimator
+from utils.images import gather_images
+
+from problem.higgs import HiggsConfig as Config
+from problem.higgs import Generator
+from problem.higgs import get_generators
+from problem.higgs import get_minimizer
+from problem.higgs.nll import LabelNLL
+
+from visual.special.higgs import plot_nll_around_min
+
+
+# SEED = None
+BENCHMARK_NAME = "Higgs-Prior"
+DIRECTORY = os.path.join(SAVING_DIR, BENCHMARK_NAME, "Likelihood_label")
+N_ITER = 3
+
+def main():
+    logger = set_logger()
+    logger.info("Hello world !")
+    os.makedirs(DIRECTORY, exist_ok=True)
+    set_plot_config()
+    args = None
+
+    config = Config()
+    results = [run(args, i_cv) for i_cv in range(N_ITER)]
+    results = pd.concat(results, ignore_index=True)
+    results.to_csv(os.path.join(DIRECTORY, 'results.csv'))
+    # EVALUATION
+    eval_table = evaluate_estimator(config.TRUE.interest_parameters_names, results)
+    print_line()
+    print_line()
+    print(eval_table)
+    print_line()
+    print_line()
+    eval_table.to_csv(os.path.join(DIRECTORY, 'evaluation.csv'))
+    gather_images(DIRECTORY)
+
+
+def run(args, i_cv):
+    logger = logging.getLogger()
+    print_line()
+    logger.info('Running iter n°{}'.format(i_cv))
+    print_line()
+    directory = os.path.join(DIRECTORY, f'cv_{i_cv}')
+    os.makedirs(directory, exist_ok=True)
+
+    config = Config()
+    seed = SEED + i_cv * 5
+    train_generator, valid_generator, test_generator = get_generators(seed)
+
+    result_table = [run_iter(i_cv, i, test_config, valid_generator, test_generator, directory) for i, test_config in enumerate(config.iter_test_config())]
+    result_table = pd.DataFrame(result_table)
+    result_table.to_csv(os.path.join(directory, 'results.csv'))
+    logger.info('Plot params')
+    param_names = config.PARAM_NAMES
+    for name in param_names:
+        plot_params(name, result_table, title='Likelihood fit', directory=directory)
+
+    return result_table
+
+
+def run_iter(i_cv, i_iter, config, valid_generator, test_generator, directory):
+    logger = logging.getLogger()
+    result_row = dict(i_cv=i_cv, i=i_iter)
+    iter_directory = os.path.join(directory, f'iter_{i_iter}')
+    os.makedirs(iter_directory, exist_ok=True)
+
+    logger.info(f"True Parameters   = {config.TRUE}")
+    suffix = f'-mu={config.TRUE.mu:1.2f}_tes={config.TRUE.tes}_jes={config.TRUE.jes}_les={config.TRUE.les}'
+    # suffix += f'_nasty_bkg={config.TRUE.nasty_bkg}_sigma_soft={config.TRUE.sigma_soft}'
+# TODO : Remove
+    logger.info(f"Calib Parameters   = {config.CALIBRATED}")
+    X_test, y_test, w_test = test_generator.generate(*config.CALIBRATED, n_samples=config.N_TESTING_SAMPLES)
+    logger.info(f" s = {w_test[y_test==1].sum()}  || b = {w_test[y_test==0].sum()} ")
+# TODO : END
+    X_test, y_test, w_test = test_generator.generate(*config.TRUE, n_samples=config.N_TESTING_SAMPLES)
+    debug_label(y_test)
+
+# TODO : Remove
+    logger.info(f" s = {w_test[y_test==1].sum()}  || b = {w_test[y_test==0].sum()} ")
+# TODO : END
+
+
+    compute_nll = LabelNLL(valid_generator, y_test, w_test, config=config)
+    plot_nll_around_min(compute_nll, config.TRUE, iter_directory, suffix)
+
+    logger.info('Prepare minuit minimizer')
+    minimizer = get_minimizer(compute_nll, config.CALIBRATED, config.CALIBRATED_ERROR)
+    result_row.update(evaluate_minuit(minimizer, config.TRUE))
+    return result_row
+
+
+def debug_label(label):
+    logger = logging.getLogger()
+    n_sig = np.sum(label==1)
+    n_bkg = np.sum(label==0)
+    logger.debug(f"nb of signal      = {n_sig}")
+    logger.debug(f"nb of backgrounds = {n_bkg}")
+
+if __name__ == '__main__':
+    main()
