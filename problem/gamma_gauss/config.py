1b9df3af6c1939676a8a8844c5d6b0600799698b
Victor Estrade <victor.estrade@lri.fr>
2020-07-16 11:21:35 +0200

    Minor config update

--- problem/gamma_gauss/config.py
+++ problem/gamma_gauss/config.py
@@ -11,7 +11,7 @@ class GGConfig():
     CALIBRATED = Parameter(rescale=1, mix=0.5)
     CALIBRATED_ERROR = Parameter(rescale=2, mix=0.5)
     TRUE = Parameter(rescale=1.2, mix=0.2)
-    RANGE = Parameter(rescale=np.linspace(0.8, 2, 20), 
+    RANGE = Parameter(rescale=np.linspace(0.8, 2, 5), 
                         mix=np.linspace(0.1, 0.9, 4))
 
     PARAM_NAMES = TRUE._fields
