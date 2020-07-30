fc40bdeaae220ccd3302e95e1b27892fa0b83c54
Victor Estrade <victor.estrade@lri.fr>
2020-07-30 12:37:57 +0200

    Fix DATA_NAME

--- benchmark/EASYHIGGS/REG-Marginal.py
+++ benchmark/EASYHIGGS/REG-Marginal.py
@@ -47,7 +47,7 @@ from archi.reducer import A1AR8MR8L1 as ARCHI
 from ..my_argparser import REG_parse_args
 
 
-DATA_NAME = 'BALANCEDHIGGS'
+DATA_NAME = 'EASYHIGGS'
 BENCHMARK_NAME = DATA_NAME+'-marginal'
 N_ITER = 3
 NCALL = 1
