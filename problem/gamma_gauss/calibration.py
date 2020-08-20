2dd216cd9b4daa2fa3607c329709e0b4d67fc080
Victor Estrade <victor.estrade@lri.fr>
2020-08-20 16:49:21 +0200

    Add random state as input

--- problem/gamma_gauss/calibration.py
+++ problem/gamma_gauss/calibration.py
@@ -10,12 +10,12 @@ from .parameter import Parameter
 from .config import GGConfig
 
 
-def param_generator():
+def param_generator(random_state=None):
     config = GGConfig()
     offset = - config.CALIBRATED.rescale / config.CALIBRATED_ERROR.rescale
     prior_rescale = stats.truncnorm(offset, 5, loc=config.CALIBRATED.rescale, scale=config.CALIBRATED_ERROR.rescale)
     prior_mix   = stats.uniform(loc=0, scale=1)
-    rescale = prior_rescale.rvs()
-    mix   = prior_mix.rvs()
+    rescale = prior_rescale.rvs(random_state=random_state)
+    mix   = prior_mix.rvs(random_state=random_state)
     return Parameter(rescale, mix)
 
