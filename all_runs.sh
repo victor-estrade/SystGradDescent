24fdca68abf2b50f3d4ff9d71f60200172b6fe77
Victor Estrade <victor.estrade@lri.fr>
2020-08-20 18:57:04 +0200

    FILE with all run commands to repeat full experimentation

---
+++ all_runs.sh
@@ -0,0 +1,17 @@
+# All_cmd
+
+# TOY 1D
+#========
+
+python run_array.py benchmark.GG.GB-Prior --gpu 1 --n-estimators 100 300 1000  --max-depth 3 5 10 --learning-rate 0.1 0.05 0.01 --xp-name GG-GB-Prior
+python run_array.py benchmark.GG.GB-Calib --gpu 1 --n-estimators 100 300 1000  --max-depth 3 5 10 --learning-rate 0.1 0.05 0.01 --xp-name GG-GB-Calib
+
+python run_array.py benchmark.GG.likelihood --gpu 1 --xp-name GG-likelihood
+python run_array.py benchmark.GG.likelihood --gpu 1 --xp-name GG-bayes
+
+python run_array.py benchmark.GG.REG-Marginal --gpu 1 --n-steps 2000 5000 --n-unit 50 100 200 500 --xp-name GG-REG-Marginal
+python run_array.py benchmark.GG.REG-Marginal --gpu 1 --n-steps 2000 --n-unit 200 --beta1 0.9 --beta2 0.999 --xp-name GG-REG-Marginal-bouncing
+
+
+
+
