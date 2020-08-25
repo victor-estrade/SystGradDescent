22fa066c262cf0f1e78c8a116acd629006ece8ec
Victor Estrade <victor.estrade@lri.fr>
2020-08-25 14:27:02 +0200

    Exclude titanic-[1-5] nodes + Minor

--- all_runs.sh
+++ all_runs.sh
@@ -7,6 +7,8 @@ python run_array.py benchmark.GG.CALIB-Rescale --gpu 1  --n-steps 1000 2000 5000
 python run_array.py benchmark.GG.GB-Prior --gpu 1 --n-estimators 100 300 1000  --max-depth 3 5 10 --learning-rate 0.1 0.05 0.01 --xp-name GG-GB-Prior
 python run_array.py benchmark.GG.GB-Calib --gpu 1 --n-estimators 100 300 1000  --max-depth 3 5 10 --learning-rate 0.1 0.05 0.01 --xp-name GG-GB-Calib
 
+python run_array.py benchmark.GG.NN-Prior --gpu 1 --n-steps 2000 5000 --n-unit 50 100 200 500 --xp-name GG-NN-Prior
+
 python run_array.py benchmark.GG.likelihood --gpu 1 --xp-name GG-likelihood
 python run_array.py benchmark.GG.likelihood --gpu 1 --xp-name GG-bayes
 
