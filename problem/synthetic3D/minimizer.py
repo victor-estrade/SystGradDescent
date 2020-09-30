f9ca372e67292d2d1d5add2e68b2ba538d5f8337
Victor Estrade <victor.estrade@lri.fr>
2020-09-30 11:05:30 +0200

    Add get_minimizer_no_nuisance()

--- problem/synthetic3D/minimizer.py
+++ problem/synthetic3D/minimizer.py
@@ -20,3 +20,12 @@ def get_minimizer(compute_nll, calibrated_param, calibrated_param_error):
                            limit_mu=(0, 1),
                           )
     return minimizer
+def get_minimizer_no_nuisance(compute_nll, calibrated_param, calibrated_param_error):
+    minimizer = iminuit.Minuit(compute_nll,
+                           errordef=ERRORDEF_NLL,
+                           mu=calibrated_param.mu,
+                           error_mu=calibrated_param_error.mu,
+                           limit_mu=(0, 1),
+                           print_level=0,
+                          )
+    return minimizer
