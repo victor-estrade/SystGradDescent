c902b571ade9e847927b2e7f4699f03aca7344d3
Victor Estrade <victor.estrade@lri.fr>
2020-09-30 11:05:06 +0200

    Update S3D2 config

--- problem/synthetic3D/config.py
+++ problem/synthetic3D/config.py
@@ -24,35 +24,24 @@ class S3D2Config():
 
     RANGE = Parameter(r=np.linspace(-0.5, 0.5, 3),
                     lam=np.linspace(2, 4, 3),
-                    mu=np.linspace(0.1, 0.99, 4),)
+                    mu=np.linspace(0.5, 2, 3),)
 
     PARAM_NAMES = TRUE.parameter_names
     INTEREST_PARAM_NAME = TRUE.interest_parameters_names
 
-    CALIBRATED_MU = CALIBRATED.mu
-    CALIBRATED_R = CALIBRATED.r
-    CALIBRATED_LAMBDA = CALIBRATED.lam
-
-    CALIBRATED_MU_ERROR = CALIBRATED_ERROR.mu
-    CALIBRATED_R_ERROR = CALIBRATED_ERROR.r
-    CALIBRATED_LAMBDA_ERROR = CALIBRATED_ERROR.lam
-    
-    TRUE_MU = TRUE.mu
-    TRUE_R = TRUE.r
-    TRUE_LAMBDA = TRUE.lam
-
-    # TRUE_MU_RANGE = np.arange(0, 0.3, 0.05)[1:]
-    # # TRUE_MU_RANGE = [100/1050, 150/1050, 200/1050]
-    # TRUE_R_RANGE = np.arange(-0.2, 0.2, 0.1)
-    # TRUE_LAMBDA_RANGE = np.arange(2.1, 3.5, 0.2)
-
     N_TRAINING_SAMPLES = 30000
     N_VALIDATION_SAMPLES = 30000
     N_TESTING_SAMPLES = 30000
+    RANGE_N_TEST = [30000,]
 
     def iter_test_config(self):
-        for true_r, true_lam, true_mu in itertools.product(*self.RANGE):
+        param_lists = [*self.RANGE ] + [ S3D2Config.RANGE_N_TEST ]
+        for true_r, true_lam, true_mu, n_test_samples in itertools.product(*param_lists):
             new_config = S3D2Config()
             new_config.TRUE = Parameter(true_r, true_lam, true_mu)
+            new_config.N_TESTING_SAMPLES = n_test_samples
             yield new_config
 
+    def iter_nuisance(self):
+        for nuisance in itertools.product(*self.FINE_RANGE.nuisance_parameters):
+            yield nuisance
