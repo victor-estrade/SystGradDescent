450c4620e7557fad8c3bedd9ae1e3a23a81f58f3
Victor Estrade <victor.estrade@lri.fr>
2020-10-01 16:49:38 +0200

    Minor

--- dl_savings.sh
+++ dl_savings.sh
@@ -11,6 +11,8 @@ rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/CO
 # rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/GG-prior/PivotClassifier2/ ./OUTPUT/GG-prior/PivotClassifier2
 # rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/S3D2-calib/Calib_lam/ ./OUTPUT/S3D2-calib/Calib_lam/
 # rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/S3D2-calib/Calib_r/ ./OUTPUT/S3D2-calib/Calib_r/
+# rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/S3D2-prior/PivotClassifier/ ./OUTPUT/S3D2-prior/PivotClassifier/
+# rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/MODELS/S3D2/PivotClassifier/ ./OUTPUT/MODELS/S3D2/PivotClassifier/
 # rsync -rtv titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/MODELS/S3D2/Calib_lam ./OUTPUT/MODELS/S3D2/Calib_lam/
 
 # rsync -rtv --include='valid_roc_concat.png' --include='*/' --exclude='*' titanic:/home/tao/vestrade/workspace/SystML/SystGradDescent/OUTPUT/GG-prior/PivotClassifier/ ./OUTPUT/GG-prior/PivotClassifier
