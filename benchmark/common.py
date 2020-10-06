9f780e0e316e554819b83a18c5dc198dd1cba819
Victor Estrade <victor.estrade@lri.fr>
2020-10-06 12:01:04 +0200

    Add an assert to ensure same number of iter in each CV

--- benchmark/common.py
+++ benchmark/common.py
@@ -29,13 +29,14 @@ def load_conditional_estimations(directory, start_cv=0, end_cv=30):
 
 
 def generate_conditional_estimations(directory, start_cv=0, end_cv=30):
+    i_cv_0 = 0
+    fname = os.path.join(directory, f"cv_{i_cv_0}", "iter_*", "no_nuisance.csv")
+    n_iter = len(glob.glob(fname))
     for i_cv in range(start_cv, end_cv):
         fname = os.path.join(directory, f"cv_{i_cv}", "iter_*", "no_nuisance.csv")
-        print("looking for subfiles")
         all_files = glob.glob(fname)
-        print(all_files)
+        assert len(all_files) == n_iter, f"found {len(all_files)} subfiles in cv_{i_cv} instead of {n_iter} in cv_{i_cv_0}"
         conditional_estimations = pd.concat([pd.read_csv(f) for f in all_files])
         conditional_estimations['i_cv'] = i_cv
         fname = os.path.join(directory, f"cv_{i_cv}", "conditional_estimations.csv")
         conditional_estimations.to_csv(fname)
-
