2e74faabbf831c5e73403777c45be1105b3b230a
Victor Estrade <victor.estrade@lri.fr>
2020-10-13 18:05:55 +0200

    Upgrade speedy

--- benchmark/HIGGS/speedy.py
+++ benchmark/HIGGS/speedy.py
@@ -14,6 +14,17 @@ from problem.higgs import GeneratorTorch
 from problem.higgs import param_generator
 from problem.higgs.higgs_geant import load_data
 
+def parse_args(main_description="assert GPU and CPU generator do the same computation"):
+    parser = argparse.ArgumentParser(description=main_description)
+
+    parser.add_argument("--verbose", "-v", type=int, choices=[0, 1, 2],
+                        default=0, help="increase output verbosity")
+
+    parser.add_argument('--no-cuda', '--no-gpu', help='flag to use or not the gpu',
+                        action='store_false', dest='cuda')
+
+    args, others = parser.parse_known_args()
+    return args
 
 def time_to_str(t):
     return f"{t: 2.5f} sec"
@@ -29,6 +40,7 @@ def measure_time(func, repeat=3):
 
 def main():
     print("hello")
+    args = parse_args()
     print("Loading data ...", end='', flush=True)
     data = load_data()
     print(".. data loaded", flush=True)
@@ -42,11 +54,11 @@ def main():
         param = param_generator()
         X, y, w = generator.generate(*param, n_samples=N_SAMPLES)
 
-    # mean_time = measure_time(cpu_generator)
-    # print(time_to_str(mean_time))
+    mean_time = measure_time(cpu_generator)
+    print(time_to_str(mean_time))
 
 
-    generator = GeneratorTorch(data, seed=42)
+    generator = GeneratorTorch(data, seed=42, cuda=args.cuda)
     def gpu_generator():
         param = param_generator()
         X, y, w = generator.generate(*param, n_samples=N_SAMPLES)
