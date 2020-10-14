730157d8f2064272ee54012ad27a43dd6c825e63
Victor Estrade <victor.estrade@lri.fr>
2020-10-14 17:43:49 +0200

    Minor add

--- all_runs.sh
+++ all_runs.sh
@@ -105,3 +105,56 @@ python run_array.py benchmark.S3D2.REG-Calib --gpu 1 --n-steps 2000 --n-unit 200
 
 
 
+
+# HIGGS
+#========
+
+# CALIBRATION / MARGINAL
+# python run_array.py benchmark.HIGGS.CALIB-LAM --gpu 1  --n-steps 1000 2000 5000 --n-unit 80 200 --xp-name HIGGS-CALIB-LAM --partition besteffort
+# python run_array.py benchmark.HIGGS.CALIB-R --gpu 1  --n-steps 1000 2000 5000 --n-unit 80 200 --xp-name HIGGS-CALIB-R --partition besteffort
+
+# python run_array.py benchmark.HIGGS.likelihood --gpu 1 --xp-name HIGGS-likelihood --partition besteffort
+# python run_array.py benchmark.HIGGS.bayes --gpu 1 --xp-name HIGGS-bayes --partition besteffort
+
+# python run_array.py benchmark.HIGGS.REG-Marginal --gpu 1 --n-steps 5000 --n-unit 200 500 --sample-size 5000 --xp-name HIGGS-REG-Marginal --partition besteffort
+# python run_array.py benchmark.HIGGS.REG-Marginal --gpu 1 --n-steps 2000 --n-unit 200 --beta1 0.9 --beta2 0.999 --xp-name HIGGS-REG-Marginal-bouncing --partition besteffort
+
+
+# PRIOR RUN
+python run_array.py benchmark.HIGGS.GB-Prior --gpu 1 --n-estimators 300  --max-depth 3 --learning-rate 0.1 --xp-name HIGGS-GB-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.GB-Prior --gpu 1 --n-estimators 300 1000  --max-depth 3 5 --learning-rate 0.1 0.05 0.01 --xp-name HIGGS-GB-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.NN-Prior --gpu 1 --n-steps 5000 --n-unit 200 500 --batch-size 1000 --xp-name HIGGS-NN-Prior --partition besteffort
+
+# python run_array.py benchmark.HIGGS.DA-Prior --gpu 1 --start-cv 0 --end-cv 6 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-DA-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.DA-Prior --gpu 1 --start-cv 6 --end-cv 12 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-DA-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.DA-Prior --gpu 1 --start-cv 12 --end-cv 18 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-DA-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.DA-Prior --gpu 1 --start-cv 18 --end-cv 24 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-DA-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.DA-Prior --gpu 1 --start-cv 24 --end-cv 30 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-DA-Prior --partition besteffort
+
+# python run_array.py benchmark.HIGGS.PIVOT-Prior --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name HIGGS-PIVOT-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.TP-Prior --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name HIGGS-TP-Prior --partition besteffort
+
+# python run_array.py benchmark.HIGGS.INF-Prior --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-INF-Prior --partition besteffort
+
+# python run_array.py benchmark.HIGGS.REG-Prior --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-REG-Prior --partition besteffort
+# python run_array.py benchmark.HIGGS.REG-Prior --gpu 1 --n-steps 2000 --n-unit 200 --beta1 0.9 --beta2 0.999 --xp-name HIGGS-REG-Prior-bouncing --partition besteffort
+
+
+# CALIB RUN
+# python run_array.py benchmark.HIGGS.GB-Calib --gpu 1 --n-estimators 300 1000  --max-depth 3 5 --learning-rate 0.1 0.05 0.01 --xp-name HIGGS-GB-Calib --partition besteffort
+# python run_array.py benchmark.HIGGS.NN-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --batch-size 1000 --xp-name HIGGS-NN-Calib --partition besteffort
+# python run_array.py benchmark.HIGGS.DA-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-DA-Calib --partition besteffort
+
+# python run_array.py benchmark.HIGGS.PIVOT-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name HIGGS-PIVOT-Calib --partition besteffort
+# python run_array.py benchmark.HIGGS.TP-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name HIGGS-TP-Calib --partition besteffort
+
+# python run_array.py benchmark.HIGGS.INF-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-INF-Calib --partition besteffort
+
+# python run_array.py benchmark.HIGGS.REG-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name HIGGS-REG-Calib --partition besteffort
+# python run_array.py benchmark.HIGGS.REG-Calib --gpu 1 --n-steps 2000 --n-unit 200 --beta1 0.9 --beta2 0.999 --xp-name HIGGS-REG-Calib-bouncing --partition besteffort
+
+
+
+
+
+python run_array.py benchmark.S3D2.GB-Prior --gpu 1 --n-estimators 300 1000  --max-depth 3 5 --learning-rate 0.1 0.05 0.01 --xp-name S3D2-GB-Prior --partition besteffort
