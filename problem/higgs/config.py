f764b362879dd10222aaa4cdd647869bf67fa856
Victor Estrade <victor.estrade@lri.fr>
2020-10-19 14:40:01 +0200

    Add HiggsConfigTESOnly

--- problem/higgs/config.py
+++ problem/higgs/config.py
@@ -9,6 +9,7 @@ import numpy as np
 from .parameter import Parameter
 from .parameter import FuturParameter
 
+
 class HiggsConfig():
     CALIBRATED = Parameter(tes=1, jes=1, les=1, mu=1)
     CALIBRATED_ERROR = Parameter(tes=0.1, jes=0.1, les=0.05, mu=1)
@@ -44,6 +45,45 @@ class HiggsConfig():
     def iter_nuisance(self):
         for nuisance in itertools.product(*self.FINE_RANGE.nuisance_parameters):
             yield nuisance
+
+
+class HiggsConfigTesOnly():
+    CALIBRATED = Parameter(tes=1, jes=1, les=1, mu=1)
+    CALIBRATED_ERROR = Parameter(tes=0.1, jes=0.1, les=0.05, mu=1)
+    TRUE = Parameter(tes=1, jes=1, les=1, mu=1)
+    RANGE = Parameter(tes=np.linspace(0.9, 1.1, 3),
+                        jes=[1.0],
+                        les=[1.0],
+                        mu=np.linspace(0.5, 2, 3))
+    
+    FINE_RANGE = Parameter(tes=np.linspace(0.9, 1.1, 15),
+                        jes=[1.0],
+                        les=[1.0],
+                        mu=[1.0])
+
+    MIN = Parameter(tes=0.9, jes=0.95, les=0.98, mu=0.1)
+    MAX = Parameter(tes=1.1, jes=1.05, les=1.02, mu=2.2)
+    PARAM_NAMES = TRUE.parameter_names
+    INTEREST_PARAM_NAME = TRUE.interest_parameters_names
+
+    N_TRAINING_SAMPLES = None
+    N_VALIDATION_SAMPLES = None
+    N_TESTING_SAMPLES = None
+    RANGE_N_TEST = [None,]
+
+    def iter_test_config(self):
+        param_lists = [*self.RANGE ] + [ HiggsConfig.RANGE_N_TEST ]
+        for tes, jes, les, mu, n_test_samples in itertools.product(*param_lists):
+            new_config = HiggsConfig()
+            new_config.TRUE = Parameter(tes, jes, les, mu)
+            new_config.N_TESTING_SAMPLES = n_test_samples
+            yield new_config
+
+    def iter_nuisance(self):
+        for nuisance in itertools.product(*self.FINE_RANGE.nuisance_parameters):
+            yield nuisance
+
+
 
 
 class FuturHiggsConfig():
