06559db8b5402e533435d75d23f211aed8ed7d2d
Victor Estrade <victor.estrade@lri.fr>
2020-10-21 15:44:13 +0200

    Add DA and NN to COMPARE.HIGGS

---
+++ benchmark/COMPARE/HIGGS/DA-Prior.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+from .visual.common import make_common_plots
+from .visual.common import make_hp_table
+from ..loader import DALoader
+from .hyper_parameters import DA_HP
+
+
+def main():
+    print("hello")
+    data_name = 'HIGGS'
+    benchmark_name = 'HIGGS-prior'
+    make_hp_table(data_name, benchmark_name, DA_HP, DALoader)
+    make_common_plots(data_name, benchmark_name, DA_HP, DALoader)
+
+    data_name = 'HIGGSTES'
+    benchmark_name = 'HIGGSTES-prior'
+    make_hp_table(data_name, benchmark_name, DA_HP, DALoader)
+    make_common_plots(data_name, benchmark_name, DA_HP, DALoader)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
