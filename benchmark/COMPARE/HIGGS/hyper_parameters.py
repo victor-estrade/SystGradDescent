a570d40eb748254a1abe4e6f1d5cb5d686e7efda
Victor Estrade <victor.estrade@lri.fr>
2020-10-21 16:14:02 +0200

    Fix HP

--- benchmark/COMPARE/HIGGS/hyper_parameters.py
+++ benchmark/COMPARE/HIGGS/hyper_parameters.py
@@ -9,7 +9,8 @@ from __future__ import unicode_literals
 DA_HP    = dict(
                 archi_name=["L4"]
                 , n_steps=[2000, 5000]
-                , n_units=[50, 100, 200, 500]
+                , n_units=[500]
+                , batch_size=[10000]
                 )
 
 
@@ -22,14 +23,15 @@ GB_HP    = dict(
 INF_HP   = dict(
                 archi_name=["L4"]
                 , n_steps=[2000, 5000]
-                , n_units=[50, 100, 200, 500]
+                , n_units=[500]
+                , batch_size=[10000]
                 )
 
 NN_HP    = dict(
                 archi_name=["L4"]
                 , n_steps=[2000, 5000]
                 , n_units=[50, 100, 200, 500]
-                , batch_size=[20, 1000]
+                , batch_size=[10000]
                 )
 
 PIVOT_HP = dict(
@@ -37,7 +39,7 @@ PIVOT_HP = dict(
                 , n_steps=[2000, 5000]
                 , n_units=[50, 100, 200, 500]
                 , trade_off=[1.0, 0.1, 1e-2, 1e-3]
-                , batch_size=[1000]
+                , batch_size=[10000]
                 )
 
 REG_HP   = dict(
@@ -57,5 +59,5 @@ TP_HP    = dict(
                 , n_steps=[2000, 5000]
                 , n_units=[50, 100, 200, 500]
                 , trade_off=[1.0, 0.1, 1e-2, 1e-3]
-                , batch_size=[1000]
+                , batch_size=[10000]
                 )
