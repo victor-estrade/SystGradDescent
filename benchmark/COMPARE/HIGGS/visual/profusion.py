95a0012902cc46e704da5c9e846c86ab246adba8
Victor Estrade <victor.estrade@lri.fr>
2020-10-29 12:32:25 +0100

    Fixed unique_alphas

--- benchmark/COMPARE/HIGGS/visual/profusion.py
+++ benchmark/COMPARE/HIGGS/visual/profusion.py
@@ -22,7 +22,11 @@ from collections import defaultdict
 def n_samples_mse(all_evaluations, title="No Title", directory=DEFAULT_DIR):
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         chosen_true_mu = 1.0  # Nominal value of mu
@@ -31,12 +35,12 @@ def n_samples_mse(all_evaluations, title="No Title", directory=DEFAULT_DIR):
             x = df.n_test_samples
             y = df.target_mse
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%n_alphas])
 
     plt.xlabel('# test samples')
     plt.ylabel("MSE $\\hat \\mu$")
     plt.title(title)
-    plt.legend([f"$\\alpha$={a}" for a in unique_alphas ], bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.legend([f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ], bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_n_samples_mse.png'), bbox_inches='tight')
     plt.clf()
 
@@ -62,7 +66,11 @@ def nominal_n_samples_mse(all_evaluations, title="No Title", directory=DEFAULT_D
 def n_samples_v_stat(all_evaluations, title="No Title", directory=DEFAULT_DIR):
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         chosen_true_mu = 1.0  # Nominal value of mu
@@ -71,12 +79,12 @@ def n_samples_v_stat(all_evaluations, title="No Title", directory=DEFAULT_DIR):
             x = df.n_test_samples
             y = df.var_stat
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%n_alphas])
 
     plt.xlabel('# test samples')
     plt.ylabel("V_stat")
     plt.title(title)
-    plt.legend([f"$\\alpha$={a}" for a in unique_alphas ], bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.legend([f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ], bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_n_samples_v_stat.png'), bbox_inches='tight')
     plt.clf()
 
@@ -102,7 +110,11 @@ def nominal_n_samples_v_stat(all_evaluations, title="No Title", directory=DEFAUL
 def n_samples_v_syst(all_evaluations, title="No Title", directory=DEFAULT_DIR):
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         chosen_true_mu = 1.0  # Nominal value of mu
@@ -111,12 +123,12 @@ def n_samples_v_syst(all_evaluations, title="No Title", directory=DEFAULT_DIR):
             x = df.n_test_samples
             y = df.var_syst
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%n_alphas])
 
     plt.xlabel('# test samples')
     plt.ylabel("V_syst")
     plt.title(title)
-    plt.legend([f"$\\alpha$={a}" for a in unique_alphas ], bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.legend([f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ], bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_n_samples_v_syst.png'), bbox_inches='tight')
     plt.clf()
 
@@ -142,7 +154,11 @@ def nominal_n_samples_v_syst(all_evaluations, title="No Title", directory=DEFAUL
 def n_samples_sigma_mean(all_evaluations, title="No Title", directory=DEFAULT_DIR):
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         chosen_true_mu = 1.0  # Nominal value of mu
@@ -151,12 +167,12 @@ def n_samples_sigma_mean(all_evaluations, title="No Title", directory=DEFAULT_DI
             x = df.n_test_samples
             y = df.sigma_mean
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.plot(x, y, 'o-', label=label, color=color_cycle[i%n_alphas])
 
     plt.xlabel('# test samples')
     plt.ylabel("average $\\hat \\sigma_{\\hat \\mu}$")
     plt.title(title)
-    plt.legend([f"$\\alpha$={a}" for a in unique_alphas ], bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.legend([f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ], bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_n_samples_sigma_mean.png'), bbox_inches='tight')
     plt.clf()
 
@@ -182,7 +198,11 @@ def nominal_n_samples_sigma_mean(all_evaluations, title="No Title", directory=DE
 def true_mu_estimator(all_evaluations, title="No Title", directory=DEFAULT_DIR):
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         max_n_test_samples = evaluation.n_test_samples.max()
@@ -193,13 +213,13 @@ def true_mu_estimator(all_evaluations, title="No Title", directory=DEFAULT_DIR):
             y_err = df.sigma_mean
             true = df.true_mu
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2, label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2, label=label, color=color_cycle[i%n_alphas])
     plt.scatter(x, true, marker='+', c='red', label='truth', s=500, zorder=3)
 
     plt.xlabel('true $\\mu$')
     plt.ylabel("average $\\hat \\mu \\pm \\sigma_{\\hat \\mu}$")
     plt.title(title)
-    plt.legend(["true",] +[f"$\\alpha$={a}" for a in unique_alphas ], bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.legend(["true",] +[f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ], bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_true_mu_estimator.png'), bbox_inches='tight')
     plt.clf()
 
@@ -207,7 +227,11 @@ def true_mu_estimator(all_evaluations, title="No Title", directory=DEFAULT_DIR):
 def true_mu_target_mean_std(all_evaluations, title="No Title", directory=DEFAULT_DIR):
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         max_n_test_samples = evaluation.n_test_samples.max()
@@ -218,13 +242,13 @@ def true_mu_target_mean_std(all_evaluations, title="No Title", directory=DEFAULT
             y_err = df.target_std
             true = df.true_mu
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2, label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2, label=label, color=color_cycle[i%n_alphas])
     plt.scatter(x, true, marker='+', c='red', label='truth', s=500, zorder=3)
 
     plt.xlabel('true $\\mu$')
     plt.ylabel("average $\\hat \\mu \\pm std(\\hat \\mu)$")
     plt.title(title)
-    plt.legend(["true",] +[f"$\\alpha$={a}" for a in unique_alphas ], bbox_to_anchor=(1.01, 1), loc='upper left')
+    plt.legend(["true",] +[f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ], bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_true_mu_target_mean_std.png'), bbox_inches='tight')
     plt.clf()
 
@@ -234,7 +258,11 @@ def true_mu_target_mean(all_evaluations, title="No Title", directory=DEFAULT_DIR
     from matplotlib.lines import Line2D
     prop_cycle = plt.rcParams['axes.prop_cycle']
     color_cycle = prop_cycle.by_key()['color']
-    unique_alphas = all_evaluations[0].true_rescale.unique()
+    unique_tes = all_evaluations[0].true_tes.unique()
+    unique_jes = all_evaluations[0].true_jes.unique()
+    unique_les = all_evaluations[0].true_les.unique()
+    unique_alphas = (unique_tes, unique_jes, unique_les)
+    n_alphas = len(unique_tes) * len(unique_jes) * len(unique_les)
 
     for evaluation in all_evaluations:
         max_n_test_samples = evaluation.n_test_samples.max()
@@ -244,17 +272,17 @@ def true_mu_target_mean(all_evaluations, title="No Title", directory=DEFAULT_DIR
             y = df.target_mean
             true = df.true_mu
             label = f"tes={true_tes}, jes={true_jes}, les={true_les}"
-            plt.scatter(x, y, marker='o', label=label, color=color_cycle[i%len(unique_alphas)])
+            plt.scatter(x, y, marker='o', label=label, color=color_cycle[i%n_alphas])
     plt.scatter(x, true, marker='+', c='red', label='truth', s=500,  zorder=3)
 
     plt.xlabel('true $\\mu$')
     plt.ylabel("average $\\hat \\mu$")
     plt.title(title)
     legend_elements = [Line2D([0], [0], marker='+', color='red', label='true', markersize=15, markeredgewidth=5)]
-    legend_elements += [Line2D([0], [0], marker='o', color=color_cycle[i%len(unique_alphas)], label=f"$\\alpha$={a}")
+    legend_elements += [Line2D([0], [0], marker='o', color=color_cycle[i%n_alphas], label=f"$\\alpha$={a}")
                         for i, a in enumerate(unique_alphas)]
     plt.legend(handles=legend_elements, bbox_to_anchor=(1.01, 1), loc='upper left')
-    # plt.legend(["true",] +[f"$\\alpha$={a}" for a in unique_alphas ]bbox_to_anchor=(1.01, 1), loc='upper left')
+    # plt.legend(["true",] +[f"tes={tes}, jes={jes}, les={les}" for tes, jes, les in zip(*unique_alphas) ]bbox_to_anchor=(1.01, 1), loc='upper left')
     plt.savefig(os.path.join(directory, f'profusion_true_mu_target_mean.png'), bbox_inches='tight')
     plt.clf()
 
