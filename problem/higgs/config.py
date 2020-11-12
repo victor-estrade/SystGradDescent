a94927c41e02fcabf398fcacdc7eb6e2ef7d4939
Victor Estrade <victor.antoine.estrade@gmail.com>
2020-11-12 15:19:14 +0100

    Reduced TES and JES in HIGGS Config

--- problem/higgs/config.py
+++ problem/higgs/config.py
@@ -12,13 +12,13 @@ from .parameter import FuturParameter
 
 class HiggsConfig():
     CALIBRATED = Parameter(tes=1.0, jes=1.0, les=1.0, mu=1.0)
-    CALIBRATED_ERROR = Parameter(tes=0.05, jes=0.05, les=0.02, mu=1.0)
+    CALIBRATED_ERROR = Parameter(tes=0.03, jes=0.03, les=0.02, mu=1.0)
     TRUE = Parameter(tes=1.0, jes=1.0, les=1.0, mu=1.0)
-    RANGE = Parameter(tes=np.linspace(0.95, 1.05, 3),
-                        jes=np.linspace(0.95, 1.05, 3),
+    RANGE = Parameter(tes=np.linspace(0.97, 1.03, 3),
+                        jes=np.linspace(0.97, 1.03, 3),
                         les=np.linspace(0.98, 1.02, 3),
-                        mu=np.linspace(0.5, 2, 3))
-    
+                        mu=np.linspace(0.5, 2, 4))
+
     FINE_RANGE = Parameter(tes=np.linspace(0.9, 1.1, 5),
                         jes=np.linspace(0.9, 1.1, 5),
                         les=np.linspace(0.9, 1.1, 5),
@@ -49,13 +49,13 @@ class HiggsConfig():
 
 class HiggsConfigTesOnly():
     CALIBRATED = Parameter(tes=1.0, jes=1.0, les=1.0, mu=1.0)
-    CALIBRATED_ERROR = Parameter(tes=0.05, jes=0.05, les=0.02, mu=1.0)
+    CALIBRATED_ERROR = Parameter(tes=0.03, jes=0.03, les=0.02, mu=1.0)
     TRUE = Parameter(tes=1.0, jes=1.0, les=1.0, mu=1.0)
-    RANGE = Parameter(tes=np.linspace(0.95, 1.05, 3),
+    RANGE = Parameter(tes=np.linspace(0.97, 1.03, 3),
                         jes=[1.0],
                         les=[1.0],
                         mu=np.linspace(0.5, 2, 4))
-    
+
     FINE_RANGE = Parameter(tes=np.linspace(0.9, 1.1, 15),
                         jes=[1.0],
                         les=[1.0],
@@ -88,9 +88,9 @@ class HiggsConfigTesOnly():
 
 class FuturHiggsConfig():
     CALIBRATED = FuturParameter(tes=1.0, jes=1.0, les=1.0, nasty_bkg=1.0, sigma_soft=0.0, mu=1.0)
-    CALIBRATED_ERROR = FuturParameter(tes=0.05, jes=0.05, les=0.02, nasty_bkg=0.5, sigma_soft=1.0, mu=1.0)
+    CALIBRATED_ERROR = FuturParameter(tes=0.03, jes=0.03, les=0.02, nasty_bkg=0.5, sigma_soft=1.0, mu=1.0)
     TRUE = FuturParameter(tes=1.0, jes=1.0, les=1.0, nasty_bkg=1.0, sigma_soft=1.0, mu=1.0)
-    RANGE = FuturParameter(tes=np.linspace(0.95, 1.05, 3), 
+    RANGE = FuturParameter(tes=np.linspace(0.97, 1.03, 3),
                         jes=[1.0],
                         les=[1.0],
                         nasty_bkg=[1.0],
