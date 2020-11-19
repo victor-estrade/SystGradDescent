abaa5d4569a82550cdfe57fdaff5bb871ca1c59d
Victor Estrade <victor.antoine.estrade@gmail.com>
2020-11-19 10:25:05 +0100

    Minor Print

--- benchmark/COMPARE/HIGGS/visual/common.py
+++ benchmark/COMPARE/HIGGS/visual/common.py
@@ -85,6 +85,7 @@ def make_individual_threshold_plots(threshold_table, loader):
 
 
 def make_common_plots(data_name, benchmark_name, args, TheLoader):
+    print(data_name, benchmark_name)
     print("Make evaluation plots")
     print("="*25)
     make_evaluation_plots(data_name, benchmark_name, args, TheLoader)
