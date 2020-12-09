eae288fe60f7aa092d0989a1d5bd765310926dc2
Victor Estrade <victor.antoine.estrade@gmail.com>
2020-12-09 10:46:35 +0100

    Rm fisher plots temporary

--- benchmark/COMPARE/HIGGS/visual/common.py
+++ benchmark/COMPARE/HIGGS/visual/common.py
@@ -102,7 +102,7 @@ def make_common_plots(data_name, benchmark_name, args, TheLoader):
     make_evaluation_plots(data_name, benchmark_name, args, TheLoader)
     print("Make fisher plots")
     print("="*25)
-    make_fisher_plots(data_name, benchmark_name, args, TheLoader)
+    # make_fisher_plots(data_name, benchmark_name, args, TheLoader)
     print("Make threshold plots")
     print("="*25, flush=True)
     make_threshold_plots(data_name, benchmark_name, args, TheLoader)
