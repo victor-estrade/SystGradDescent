d42efb48f004ece8967207d4d51832893c9f7736
Victor Estrade <victor.antoine.estrade@gmail.com>
2020-12-09 17:55:30 +0100

    Reput fisher gain

--- benchmark/COMPARE/HIGGS/visual/common.py
+++ benchmark/COMPARE/HIGGS/visual/common.py
@@ -103,7 +103,7 @@ def make_common_plots(data_name, benchmark_name, args, TheLoader):
     print("Make fisher plots")
     print("="*25)
     # TODO try catch on all make_plots
-    # make_fisher_plots(data_name, benchmark_name, args, TheLoader)
+    make_fisher_plots(data_name, benchmark_name, args, TheLoader)
     print("Make threshold plots")
     print("="*25, flush=True)
     make_threshold_plots(data_name, benchmark_name, args, TheLoader)
