b8d4c364034e3420e18457cf618b109a3dfe279f
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-02-24 14:55:39 +0100

    Cleaner code

--- benchmark/COMPARE/GG/compare_models.py
+++ benchmark/COMPARE/GG/compare_models.py
@@ -38,256 +38,10 @@ import matplotlib.pyplot as plt
 from config import DEFAULT_DIR
 from config import SAVING_DIR
 
+from .visual import compare
+
 BENCHMARK_NAME =  "COMPARE"
 
-
-def best_average_mse_box_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_average_N={n_test_samples}"
-        mse = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
-            mse.append(best_mse_evaluation.target_mse)
-            methods.append(code_name)
-        plt.boxplot(mse, labels=methods)
-        plt.xticks(rotation=90)
-        plt.xlabel('method')
-        plt.ylabel("MSE $\\hat \\mu$")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_mse.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_average_mse_err_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_average_N={n_test_samples}"
-        mse = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
-            mse.append(best_mse_evaluation.target_mse)
-            methods.append(code_name)
-        x = list( range(len(methods)) )
-        y = [v.mean() for v in mse]
-        y_err = [v.std() for v in mse]
-        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
-        plt.xticks( ticks=x, labels=methods, rotation='vertical')
-        plt.xlabel('method')
-        plt.ylabel("Average MSE $\\hat \\mu \pm std $")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_mse.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_average_v_stat_box_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_average_N={n_test_samples}"
-        v_stat = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
-            v_stat.append(best_mse_evaluation.var_stat)
-            methods.append(code_name)
-        plt.boxplot(v_stat, labels=methods)
-        plt.xticks(rotation=90)
-        plt.xlabel('method')
-        plt.ylabel("V_stat")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_stat.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_average_v_stat_err_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_average_N={n_test_samples}"
-        v_stat = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
-            v_stat.append(best_mse_evaluation.var_stat)
-            methods.append(code_name)
-        x = list( range(len(methods)) )
-        y = [v.mean() for v in v_stat]
-        y_err = [v.std() for v in v_stat]
-        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
-        plt.xticks( ticks=x, labels=methods, rotation='vertical')
-        plt.xlabel('method')
-        plt.ylabel("Average V_stat $\pm std$")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_stat.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_average_v_syst_box_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_average_N={n_test_samples}"
-        v_syst = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
-            v_syst.append(best_mse_evaluation.var_syst)
-            methods.append(code_name)
-        plt.boxplot(v_syst, labels=methods)
-        plt.xticks(rotation=90)
-        plt.xlabel('method')
-        plt.ylabel("V_syst")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_syst.png'), bbox_inches="tight")
-        plt.clf()
-
-
-
-def best_average_v_syst_err_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_average_N={n_test_samples}"
-        v_syst = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
-            v_syst.append(best_mse_evaluation.var_syst)
-            methods.append(code_name)
-        x = list( range(len(methods)) )
-        y = [v.mean() for v in v_syst]
-        y_err = [v.std() for v in v_syst]
-        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
-        plt.xticks( ticks=x, labels=methods, rotation='vertical')
-        plt.xlabel('method')
-        plt.ylabel("Average V_syst $\pm std$")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_syst.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_median_mse_box_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_median_N={n_test_samples}"
-        mse = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
-            mse.append(best_mse_evaluation.target_mse)
-            methods.append(code_name)
-        plt.boxplot(mse, labels=methods)
-        plt.xticks(rotation=90)
-        plt.xlabel('method')
-        plt.ylabel("MSE $\\hat \\mu$")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_mse.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_median_mse_err_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_median_N={n_test_samples}"
-        mse = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
-            mse.append(best_mse_evaluation.target_mse)
-            methods.append(code_name)
-        x = list( range(len(methods)) )
-        y = [v.mean() for v in mse]
-        y_err = [v.std() for v in mse]
-        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
-        plt.xticks( ticks=x, labels=methods, rotation='vertical')
-        plt.xlabel('method')
-        plt.ylabel("Average MSE $\\hat \\mu \pm std $")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_mse.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_median_v_stat_box_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_median_N={n_test_samples}"
-        v_stat = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
-            v_stat.append(best_mse_evaluation.var_stat)
-            methods.append(code_name)
-        plt.boxplot(v_stat, labels=methods)
-        plt.xticks(rotation=90)
-        plt.xlabel('method')
-        plt.ylabel("V_stat")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_stat.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_median_v_stat_err_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_median_N={n_test_samples}"
-        v_stat = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
-            v_stat.append(best_mse_evaluation.var_stat)
-            methods.append(code_name)
-        x = list( range(len(methods)) )
-        y = [v.mean() for v in v_stat]
-        y_err = [v.std() for v in v_stat]
-        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
-        plt.xticks( ticks=x, labels=methods, rotation='vertical')
-        plt.xlabel('method')
-        plt.ylabel("Average V_stat $\pm std$")
-        plt.xlabel('method')
-        plt.ylabel("Average V_stat $\pm std$")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_stat.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_median_v_syst_box_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_median_N={n_test_samples}"
-        v_syst = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
-            v_syst.append(best_mse_evaluation.var_syst)
-            methods.append(code_name)
-        plt.boxplot(v_syst, labels=methods)
-        plt.xticks(rotation=90)
-        plt.xlabel('method')
-        plt.ylabel("V_syst")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_syst.png'), bbox_inches="tight")
-        plt.clf()
-
-
-def best_median_v_syst_err_plot(data, title="No Title", directory=DEFAULT_DIR):
-    for n_test_samples, df in data.groupby("n_test_samples"):
-        plot_title = f"{title}_best_median_N={n_test_samples}"
-        v_syst = []
-        methods = []
-        for code_name, all_evaluation in df.groupby("code_name"):
-            full_name, best_mse_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
-            v_syst.append(best_mse_evaluation.var_syst)
-            methods.append(code_name)
-        x = list( range(len(methods)) )
-        y = [v.mean() for v in v_syst]
-        y_err = [v.std() for v in v_syst]
-        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
-        plt.xticks( ticks=x, labels=methods, rotation='vertical')
-        plt.xlabel('method')
-        plt.ylabel("Average V_syst $\pm std$")
-        plt.title(plot_title)
-        # plt.legend()
-        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_syst.png'), bbox_inches="tight")
-        plt.clf()
 
 
 
@@ -360,7 +114,7 @@ def main():
         marginal_eval = pd.concat(marginal_eval, sort=False)
         marginal_eval['base_name'] = "Marginal"
         marginal_eval['code_name'] = "Blind-REG"
-    
+
     benchmark_name = 'GG-calib'
     all_data = load_all_data(ALL_HP, ALL_LOADER, ALL_NAME, benchmark_name=benchmark_name)
     data = pd.concat(all_data, sort=False)
@@ -368,21 +122,22 @@ def main():
 
     directory = os.path.join(SAVING_DIR, BENCHMARK_NAME, benchmark_name, "BEST_MSE")
     os.makedirs(directory, exist_ok=True)
-    best_average_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_average_v_stat_box_plot(data, title=benchmark_name, directory=directory)
-    best_average_v_syst_box_plot(data, title=benchmark_name, directory=directory)
-    best_average_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_average_v_stat_err_plot(data, title=benchmark_name, directory=directory)
-    best_average_v_syst_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_average_sigma_mean_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_v_stat_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_v_syst_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_average_v_stat_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_v_syst_err_plot(data, title=benchmark_name, directory=directory)
 
     directory = os.path.join(SAVING_DIR, BENCHMARK_NAME, benchmark_name, "BEST_MEDIAN")
     os.makedirs(directory, exist_ok=True)
-    best_median_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_median_v_stat_box_plot(data, title=benchmark_name, directory=directory)
-    best_median_v_syst_box_plot(data, title=benchmark_name, directory=directory)
-    best_median_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_median_v_stat_err_plot(data, title=benchmark_name, directory=directory)
-    best_median_v_syst_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_median_v_stat_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_v_syst_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_median_v_stat_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_v_syst_err_plot(data, title=benchmark_name, directory=directory)
 
     # PRIOR PLOTS
 
@@ -393,21 +148,22 @@ def main():
 
     directory = os.path.join(SAVING_DIR, BENCHMARK_NAME, benchmark_name, "BEST_MSE")
     os.makedirs(directory, exist_ok=True)
-    best_average_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_average_v_stat_box_plot(data, title=benchmark_name, directory=directory)
-    best_average_v_syst_box_plot(data, title=benchmark_name, directory=directory)
-    best_average_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_average_v_stat_err_plot(data, title=benchmark_name, directory=directory)
-    best_average_v_syst_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_average_sigma_mean_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_v_stat_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_v_syst_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_average_v_stat_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_average_v_syst_err_plot(data, title=benchmark_name, directory=directory)
 
     directory = os.path.join(SAVING_DIR, BENCHMARK_NAME, benchmark_name, "BEST_MEDIAN")
     os.makedirs(directory, exist_ok=True)
-    best_median_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_median_v_stat_box_plot(data, title=benchmark_name, directory=directory)
-    best_median_v_syst_box_plot(data, title=benchmark_name, directory=directory)
-    best_median_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
-    best_median_v_stat_err_plot(data, title=benchmark_name, directory=directory)
-    best_median_v_syst_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_mse_box_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_median_v_stat_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_v_syst_box_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_mse_err_plot(data_and_marginal, title=benchmark_name, directory=directory)
+    compare.best_median_v_stat_err_plot(data, title=benchmark_name, directory=directory)
+    compare.best_median_v_syst_err_plot(data, title=benchmark_name, directory=directory)
 
 
 
---
+++ benchmark/COMPARE/GG/visual/compare.py
@@ -0,0 +1,241 @@
+# coding: utf-8
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
+
+import matplotlib.pyplot as plt
+from config import DEFAULT_DIR
+from config import SAVING_DIR
+
+def max_target_mse_mean(all_evaluation):
+    full_name, best_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.mean())
+    return full_name, best_evaluation
+
+def max_target_mse_median(all_evaluation):
+    full_name, best_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].target_mse.median())
+    return full_name, best_evaluation
+
+def min_sigma_mean_mean(all_evaluation):
+    full_name, best_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].sigma_mean.mean())
+    return full_name, best_evaluation
+
+def min_sigma_mean_median(all_evaluation):
+    full_name, best_evaluation = max(all_evaluation.groupby('model_full_name'), key=lambda t : t[1].sigma_mean.median())
+    return full_name, best_evaluation
+
+
+def extract(df, value_name, criterion):
+    values = []
+    methods = []
+    for code_name, all_evaluation in df.groupby("code_name"):
+        full_name, best_evaluation = criterion(all_evaluation)
+        values.append(best_evaluation[value_name])
+        methods.append(code_name)
+    return values, methods
+
+def best_average_mse_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        mse, methods = extract(df, "target_mse", max_target_mse_mean)
+        plt.boxplot(mse, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("MSE $\\hat \\mu$")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_mse.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_average_mse_err_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        mse, methods = extract(df, "target_mse", max_target_mse_mean)
+        x = list( range(len(methods)) )
+        y = [v.mean() for v in mse]
+        y_err = [v.std() for v in mse]
+        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
+        plt.xticks( ticks=x, labels=methods, rotation='vertical')
+        plt.xlabel('method')
+        plt.ylabel("Average MSE $\\hat \\mu \pm std $")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_mse.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_average_sigma_mean_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        sigma, methods = extract(df, "sigma_mean", max_target_mse_mean)
+        plt.boxplot(sigma, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("average $\\hat \\sigma_{\\hat \\mu}$")
+        plt.ylim(top=0.2, bottom=0.0)
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_sigma_mean.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_average_v_stat_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        v_stat, methods = extract(df, "var_stat", max_target_mse_mean)
+        plt.boxplot(v_stat, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("V_stat")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_stat.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_average_v_stat_err_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        v_stat, methods = extract(df, "var_stat", max_target_mse_mean)
+        x = list( range(len(methods)) )
+        y = [v.mean() for v in v_stat]
+        y_err = [v.std() for v in v_stat]
+        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
+        plt.xticks( ticks=x, labels=methods, rotation='vertical')
+        plt.xlabel('method')
+        plt.ylabel("Average V_stat $\pm std$")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_stat.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_average_v_syst_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        v_syst, methods = extract(df, "var_syst", max_target_mse_mean)
+        plt.boxplot(v_syst, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("V_syst")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_syst.png'), bbox_inches="tight")
+        plt.clf()
+
+
+
+def best_average_v_syst_err_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_average_N={n_test_samples}"
+        v_syst, methods = extract(df, "var_syst", max_target_mse_mean)
+        x = list( range(len(methods)) )
+        y = [v.mean() for v in v_syst]
+        y_err = [v.std() for v in v_syst]
+        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
+        plt.xticks( ticks=x, labels=methods, rotation='vertical')
+        plt.xlabel('method')
+        plt.ylabel("Average V_syst $\pm std$")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_syst.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_median_mse_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_median_N={n_test_samples}"
+        mse, methods = extract(df, "target_mse", max_target_mse_median)
+        plt.boxplot(mse, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("MSE $\\hat \\mu$")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_mse.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_median_mse_err_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_median_N={n_test_samples}"
+        mse, methods = extract(df, "target_mse", max_target_mse_median)
+        x = list( range(len(methods)) )
+        y = [v.mean() for v in mse]
+        y_err = [v.std() for v in mse]
+        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
+        plt.xticks( ticks=x, labels=methods, rotation='vertical')
+        plt.xlabel('method')
+        plt.ylabel("Average MSE $\\hat \\mu \pm std $")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_mse.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_median_v_stat_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_median_N={n_test_samples}"
+        v_stat, methods = extract(df, "var_stat", max_target_mse_median)
+        plt.boxplot(v_stat, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("V_stat")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_stat.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_median_v_stat_err_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_median_N={n_test_samples}"
+        v_stat, methods = extract(df, "var_stat", max_target_mse_median)
+        x = list( range(len(methods)) )
+        y = [v.mean() for v in v_stat]
+        y_err = [v.std() for v in v_stat]
+        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
+        plt.xticks( ticks=x, labels=methods, rotation='vertical')
+        plt.xlabel('method')
+        plt.ylabel("Average V_stat $\pm std$")
+        plt.xlabel('method')
+        plt.ylabel("Average V_stat $\pm std$")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_stat.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_median_v_syst_box_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_median_N={n_test_samples}"
+        v_syst, methods = extract(df, "var_syst", max_target_mse_median)
+        plt.boxplot(v_syst, labels=methods)
+        plt.xticks(rotation=90)
+        plt.xlabel('method')
+        plt.ylabel("V_syst")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-boxplot_v_syst.png'), bbox_inches="tight")
+        plt.clf()
+
+
+def best_median_v_syst_err_plot(data, title="No Title", directory=DEFAULT_DIR):
+    for n_test_samples, df in data.groupby("n_test_samples"):
+        plot_title = f"{title}_best_median_N={n_test_samples}"
+        v_syst, methods = extract(df, "var_syst", max_target_mse_median)
+        x = list( range(len(methods)) )
+        y = [v.mean() for v in v_syst]
+        y_err = [v.std() for v in v_syst]
+        plt.errorbar(x, y, yerr=y_err, fmt='o', capsize=15, capthick=2)
+        plt.xticks( ticks=x, labels=methods, rotation='vertical')
+        plt.xlabel('method')
+        plt.ylabel("Average V_syst $\pm std$")
+        plt.title(plot_title)
+        # plt.legend()
+        plt.savefig(os.path.join(directory, f'{plot_title}-errplot_v_syst.png'), bbox_inches="tight")
+        plt.clf()
