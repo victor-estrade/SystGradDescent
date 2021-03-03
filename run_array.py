2dcca1578ae8b280ec265e144b00ce69d32375b0
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-03-03 12:52:27 +0100

    exclude republic-6

--- run_array.py
+++ run_array.py
@@ -166,7 +166,7 @@ SBATCH_TEMPLATE = \
 #SBATCH --mem={memory}
 #SBATCH --partition={partition}
 #SBATCH --gres=gpu:{gpu}
-#SBATCH --exclude=baltic-1,republic-3,titanic-1,republic-1
+#SBATCH --exclude=baltic-1,republic-3,titanic-1,republic-1,republic-6
 
 hostname
 
