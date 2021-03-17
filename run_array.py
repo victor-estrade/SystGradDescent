0ae065b0db84a77023378eecf8139b53a008810d
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-03-18 00:57:46 +0100

    exclude titanic-1 no space left on device

--- run_array.py
+++ run_array.py
@@ -166,7 +166,7 @@ SBATCH_TEMPLATE = \
 #SBATCH --mem={memory}
 #SBATCH --partition={partition}
 #SBATCH --gres=gpu:{gpu}
-#SBATCH --exclude=baltic-1,republic-3,republic-1,titanic-4
+#SBATCH --exclude=baltic-1,republic-3,republic-1,titanic-4,titanic-1
 
 hostname
 
