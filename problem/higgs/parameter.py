fa0890b7e8e1a68dddae058357949ebc63080621
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-04-06 10:48:32 +0200

    Refactor problem.higgs.parameter with many more Parameter dataclasses to tackle more nuisance parameter combination

--- problem/higgs/parameter.py
+++ problem/higgs/parameter.py
@@ -4,18 +4,19 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import dataclasses as dc
+
 from dataclasses import dataclass
 from dataclasses import astuple
 from dataclasses import asdict
 
-@dataclass(frozen=True)
-class MonoParameter:
-    tes : float
-    mu : float
+from dataclasses import make_dataclass
 
+
+class BaseParameter:
     @property
     def nuisance_parameters(self):
-        return (self.tes, )
+        return dc.astuple(self)[:-1]
 
     @property
     def interest_parameters(self):
@@ -23,97 +24,91 @@ class MonoParameter:
 
     @property
     def parameter_names(self):
-        return ('tes', 'mu')
+        return [field.name for field in dc.fields(self)]
 
     @property
     def nuisance_parameters_names(self):
-        return ('tes', )
+        return [field.name for field in dc.fields(self)[:-1]]
 
     @property
     def interest_parameters_names(self):
         return "mu"
 
     def __iter__(self):
-        return iter(astuple(self))
+        return iter(dc.astuple(self))
 
-    def clone_with(self, tes=None, mu=None):
-        tes = self.tes if tes is None else tes
-        mu  = self.mu if mu is None else mu
-        new_parameter = Parameter(tes, mu)
+    def clone_with(self, **changes):
+        new_parameter = dc.replace(self, **changes)
         return new_parameter
 
     def __getitem__(self, key):
-        return asdict(self)[key]
+        return dc.asdict(self)[key]
 
     def items(self):
-        return asdict(self).items()
+        return dc.asdict(self).items()
 
     def to_dict(self, prefix='', suffix=''):
         if not prefix and not suffix:
-            return asdict(self)
+            return dc.asdict(self)
         else:
             return {prefix+key+suffix : value for key, value in self.items()}
 
     def to_tuple(self):
-        return astuple(self)
+        return dc.astuple(self)
+
 
 
 @dataclass(frozen=True)
-class Parameter:
+class ParameterTes(BaseParameter):
+    tes : float
+    mu : float
+
+
+@dataclass(frozen=True)
+class ParameterJes(BaseParameter):
+    jes : float
+    mu : float
+
+
+@dataclass(frozen=True)
+class ParameterLes(BaseParameter):
+    les : float
+    mu : float
+
+
+@dataclass(frozen=True)
+class ParameterTesJes(BaseParameter):
+    tes : float
+    jes : float
+    mu : float
+
+
+@dataclass(frozen=True)
+class ParameterTesLes(BaseParameter):
+    tes : float
+    les : float
+    mu : float
+
+
+@dataclass(frozen=True)
+class ParameterTesJesLes(BaseParameter):
     tes : float
     jes : float
     les : float
     mu : float
 
-    @property
-    def nuisance_parameters(self):
-        return (self.tes, self.jes, self.les)
 
-    @property
-    def interest_parameters(self):
-        return self.mu
-
-    @property
-    def parameter_names(self):
-        return ('tes', 'jes', 'les', 'mu')
-
-    @property
-    def nuisance_parameters_names(self):
-        return ('tes', 'jes', 'les')
-
-    @property
-    def interest_parameters_names(self):
-        return "mu"
-
-    def __iter__(self):
-        return iter(astuple(self))
-
-    def clone_with(self, tes=None, jes=None, les=None, mu=None):
-        tes = self.tes if tes is None else tes
-        jes = self.jes if jes is None else jes
-        les = self.les if les is None else les
-        mu  = self.mu if mu is None else mu
-        new_parameter = Parameter(tes, jes, les, mu)
-        return new_parameter
-
-    def __getitem__(self, key):
-        return asdict(self)[key]
-
-    def items(self):
-        return asdict(self).items()
-
-    def to_dict(self, prefix='', suffix=''):
-        if not prefix and not suffix:
-            return asdict(self)
-        else:
-            return {prefix+key+suffix : value for key, value in self.items()}
-
-    def to_tuple(self):
-        return astuple(self)
+# For backward compatibility
+@dataclass(frozen=True)
+class Parameter(BaseParameter):
+    tes : float
+    jes : float
+    les : float
+    mu : float
 
 
 @dataclass(frozen=True)
-class FuturParameter:
+class FuturParameter(BaseParameter):
     tes : float
     jes : float
     les : float
@@ -121,50 +116,25 @@ class FuturParameter:
     sigma_soft : float
     mu : float
 
-    @property
-    def nuisance_parameters(self):
-        return (self.tes, self.jes, self.les, self.nasty_bkg, self.sigma_soft)
 
-    @property
-    def interest_parameters(self):
-        return self.mu
 
-    @property
-    def parameter_names(self):
-        return ('tes', 'jes', 'les', 'nasty_bkg', 'sigma_soft', 'mu')
+ALL_PARAMETER_DICT = {
+    'Tes' = ParameterTes,
+    'Jes' = ParameterJes,
+    'Les' = ParameterLes,
+    'TesJes' = ParameterTesJes,
+    'TesLes' = ParameterTesLes,
+    'TesJesLes' = ParameterTesJesLes,
+}
 
-    @property
-    def nuisance_parameters_names(self):
-        return ('tes', 'jes', 'les', 'nasty_bkg', 'sigma_soft')
 
-    @property
-    def interest_parameters_names(self):
-        return "mu"
+def get_parameter_class(tes=True, jes=False, les=False):
+    key = ''
+    if tes : key += 'Tes'
+    if jes : key += 'Jes'
+    if les : key += 'Les'
 
-    def __iter__(self):
-        return iter(astuple(self))
-
-    def clone_with(self, tes=None, jes=None, les=None, nasty_bkg=None, sigma_soft=None, mu=None):
-        tes = self.tes if tes is None else tes
-        jes = self.jes if jes is None else jes
-        les = self.les if les is None else les
-        nasty_bkg = self.nasty_bkg if nasty_bkg is None else nasty_bkg
-        sigma_soft = self.sigma_soft if sigma_soft is None else sigma_soft
-        mu  = self.mu if mu is None else mu
-        new_parameter = Parameter(tes, jes, les, nasty_bkg, sigma_soft, mu)
-        return new_parameter
-
-    def __getitem__(self, key):
-        return asdict(self)[key]
-
-    def items(self):
-        return asdict(self).items()
-
-    def to_dict(self, prefix='', suffix=''):
-        if not prefix and not suffix:
-            return asdict(self)
-        else:
-            return {prefix+key+suffix : value for key, value in self.items()}
-
-    def to_tuple(self):
-        return astuple(self)
+    if key in ALL_PARAMETER_DICT :
+        return ALL_PARAMETER_DICT[key]
+    else:
+        raise ValueError(f"Nuisance parameter combination not implemented yet tes={tes}, jes={jes}, les={les}")
