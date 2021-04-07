fd1679e67dcdb2029a5d7d018046e5d78fcb2a37
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-04-07 17:05:47 +0200

    Exclude titanic-1 and titanic-3 again

--- run_array.py
+++ run_array.py
@@ -168,7 +168,7 @@ SBATCH_TEMPLATE = \
 #SBATCH --mem={memory}
 #SBATCH --partition={partition}
 #SBATCH --gres=gpu:{gpu}
-#SBATCH --exclude=baltic-1,republic-3,republic-1,titanic-4,titanic-2,titanic-5
+#SBATCH --exclude=baltic-1,republic-3,republic-1,titanic-4,titanic-1,titanic-3,titanic-2,titanic-5
 
 # test --exclude=baltic-1,republic-[1-2],republic-[4-6],titanic-[1-5]
 # republic-3 is working  !
