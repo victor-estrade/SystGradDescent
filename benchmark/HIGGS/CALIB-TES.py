11905a4ae039951c592fe5b90c35917552920424
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-04-19 22:05:14 +0200

    comment

--- benchmark/HIGGS/CALIB-TES.py
+++ benchmark/HIGGS/CALIB-TES.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 # Command line :
-# python -m benchmark.HIGGSTES.CALIB-TES
+# python -m benchmark.HIGGS.CALIB-TES
 
 import os
 import logging
