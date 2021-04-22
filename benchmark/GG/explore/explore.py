3e6e10987d492fda19bdccd02211f19d00d179bf
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-04-22 22:07:55 +0200

    Minor

--- benchmark/GG/explore/explore.py
+++ benchmark/GG/explore/explore.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 # Command line :
-# python -m benchmark.GG.explore
+# python -m benchmark.GG.explore.explore
 
 import os
 import numpy as np
