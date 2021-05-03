d62a6473b9f85ea038c823060b5506d3576a675e
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-05-03 16:55:58 +0200

    removed precision argument

--- problem/gamma_gauss/minimizer.py
+++ problem/gamma_gauss/minimizer.py
@@ -23,7 +23,7 @@ def get_minimizer(compute_nll, calibrated_param, calibrated_param_error, toleran
                         ]
     minimizer.tol = tolerance  # Should I increase tolerance to help ???? (default is 0.1 according to doc)
     minimizer.throw_nan = True
-    minimizer.precision = 1e-12
+    # minimizer.precision = 1e-12
     return minimizer
 
 
