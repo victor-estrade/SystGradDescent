6d0e54ce617cc9e12f0712a32036e0ec9c481946
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-05-11 19:18:09 +0200

    Add precision to 1e-6 and remove debug prints

--- problem/gamma_gauss/nll.py
+++ problem/gamma_gauss/nll.py
@@ -37,5 +37,5 @@ class GGNLL():
         data_nll = np.sum(poisson_nll(self.test_summaries, rate))
         rescale_constraint = gauss_nll(rescale, config.CALIBRATED.rescale, config.CALIBRATED_ERROR.rescale)
         total_nll = data_nll + rescale_constraint
-        print(f"{rescale}, {mu}, {total_nll}", file=sys.stderr)
+        # print(f"{rescale}, {mu}, {total_nll}", file=sys.stderr)
         return total_nll
