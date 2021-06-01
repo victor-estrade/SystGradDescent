345533e64d8bd1deb47c0525ddb843932bc5f0b5
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-06-01 15:13:59 +0200

    update S3D2 minimizer parameters

--- problem/synthetic3D/minimizer.py
+++ problem/synthetic3D/minimizer.py
@@ -6,26 +6,29 @@ from __future__ import absolute_import
 import iminuit
 ERRORDEF_NLL = 0.5
 
-def get_minimizer(compute_nll, calibrated_param, calibrated_param_error):
-    MIN_VALUE = 0.01
-    MAX_VALUE = 10
+def get_minimizer(compute_nll, calibrated_param, calibrated_param_error, tolerance=0.1):
+    MIN_VALUE = 0.0001
+    MAX_VALUE = None
     minimizer = iminuit.Minuit(compute_nll,
                            r=calibrated_param.r,
                            lam=calibrated_param.lam,
                            mu=calibrated_param.mu,
                           )
     minimizer.errordef = iminuit.Minuit.LIKELIHOOD
-    minimizer.limits = [(0, None), (MIN_VALUE, None), (MIN_VALUE, MAX_VALUE)]
+    minimizer.limits = [(None, None), (MIN_VALUE, None), (MIN_VALUE, MAX_VALUE)]
     minimizer.errors = [calibrated_param_error.r
                         ,calibrated_param_error.lam
                         ,calibrated_param_error.mu
                         ]
+    minimizer.tol = tolerance  # Should I increase tolerance to help ???? (default is 0.1 according to doc)
+    minimizer.throw_nan = True
+    minimizer.precision = 1e-6
     return minimizer
 
 
 def get_minimizer_no_nuisance(compute_nll, calibrated_param, calibrated_param_error):
-    MIN_VALUE = 0.01
-    MAX_VALUE = 10
+    MIN_VALUE = 0.0
+    MAX_VALUE = None
     minimizer = iminuit.Minuit(compute_nll,
                            mu=calibrated_param.mu,
                            print_level=0,
