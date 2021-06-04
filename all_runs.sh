58987e1d153eed1e01be62be755a2fe1087fb1fe
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-06-04 16:51:33 +0200

    remove --retrain

--- all_runs.sh
+++ all_runs.sh
@@ -180,17 +180,17 @@ python run_array.py benchmark.S3D2.REG-Prior --retrain --gpu 1 --n-steps 2000 --
 
 # CALIB RUN
 python run_array.py benchmark.S3D2.FF-Calib --gpu 1 --feature-id 0 1 2 --xp-name S3D2-FF-Calib --partition besteffort --estimate-only
-python run_array.py benchmark.S3D2.GB-Calib --retrain --gpu 1 --n-estimators 300 1000  --max-depth 3 5 --learning-rate 0.1 0.05 0.01 --xp-name S3D2-GB-Calib --partition besteffort --estimate-only
-python run_array.py benchmark.S3D2.NN-Calib --retrain --gpu 1 --n-steps 5000 --n-unit 200 500 --batch-size 1000 --xp-name S3D2-NN-Calib --partition besteffort --estimate-only
-python run_array.py benchmark.S3D2.DA-Calib --retrain --gpu 1 --n-steps 5000 --n-unit 200 500 --batch-size 1000 --xp-name S3D2-DA-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.GB-Calib --gpu 1 --n-estimators 300 1000  --max-depth 3 5 --learning-rate 0.1 0.05 0.01 --xp-name S3D2-GB-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.NN-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --batch-size 1000 --xp-name S3D2-NN-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.DA-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --batch-size 1000 --xp-name S3D2-DA-Calib --partition besteffort --estimate-only
 
-python run_array.py benchmark.S3D2.PIVOT-Calib --retrain --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name S3D2-PIVOT-Calib --partition besteffort --estimate-only
-python run_array.py benchmark.S3D2.TP-Calib --retrain --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name S3D2-TP-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.PIVOT-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name S3D2-PIVOT-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.TP-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --trade-off 1 0.1 1e-2 1e-3 --xp-name S3D2-TP-Calib --partition besteffort --estimate-only
 
-python run_array.py benchmark.S3D2.INF-Calib --retrain --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name S3D2-INF-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.INF-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name S3D2-INF-Calib --partition besteffort --estimate-only
 
-python run_array.py benchmark.S3D2.REG-Calib --retrain --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name S3D2-REG-Calib --partition besteffort --estimate-only
-python run_array.py benchmark.S3D2.REG-Calib --retrain --gpu 1 --n-steps 2000 --n-unit 200 --beta1 0.9 --beta2 0.999 --xp-name S3D2-REG-Calib-bouncing --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.REG-Calib --gpu 1 --n-steps 5000 --n-unit 200 500 --xp-name S3D2-REG-Calib --partition besteffort --estimate-only
+python run_array.py benchmark.S3D2.REG-Calib --gpu 1 --n-steps 2000 --n-unit 200 --beta1 0.9 --beta2 0.999 --xp-name S3D2-REG-Calib-bouncing --partition besteffort --estimate-only
 
 
 
