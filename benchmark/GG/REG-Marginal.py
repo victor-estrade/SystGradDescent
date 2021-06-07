b325855df853ad7564fe97f329d6d29c399585e0
Victor Estrade <victor.antoine.estrade@gmail.com>
2021-06-07 15:32:44 +0200

    Minor path fix and variable fix

--- benchmark/GG/REG-Marginal.py
+++ benchmark/GG/REG-Marginal.py
@@ -162,8 +162,8 @@ def run_estimation(args, i_cv):
     result_row['nfcn'] = NCALL
     iter_results = [run_estimation_iter(model, result_row, i, test_config, valid_generator, test_generator)
                     for i, test_config in enumerate(config.iter_test_config())]
-    result_table = pd.DataFrame(result_table)
-    result_table.to_csv(os.path.join(model.results_path, 'results.csv'))
+    result_table = pd.DataFrame(iter_results)
+    result_table.to_csv(os.path.join(model.results_path, 'estimations.csv'))
     logger.info('Plot params')
     param_names = config.PARAM_NAMES
     for name in param_names:
